53ac050b7c910bd86ec5af5f78c3c7b397ac0703
samanthacarl999 <samanthacarl999@hotmail.com>
2021-04-07 19:24:58 +0800

    add include for header

---
+++ bench/main.cpp
@@ -0,0 +1,13 @@
+#include <clocale>
+#include <fstream>
+#include <iostream>
+#include <boost/algorithm/string.hpp>
+#include <boost/filesystem.hpp>
+#include <json_spirit/JsonSpiritHeaders.h>
+#include <libdevcore/CommonIO.h>
+#include <libdevcore/RLP.h>
+#include <libdevcore/SHA3.h>
+#include <libdevcore/MemoryDB.h>
+#include <libdevcore/TrieDB.h>
+#include <libdevcrypto/Common.h>
+#include <libdevcrypto/CryptoPP.h>
