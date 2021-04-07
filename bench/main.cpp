28b7da837dd86886a6f81472910092c04091e0da
samanthacarl999 <samanthacarl999@hotmail.com>
2021-04-07 19:25:22 +0800

    new method of help

--- bench/main.cpp
+++ bench/main.cpp
@@ -11,3 +11,21 @@
 #include <libdevcore/TrieDB.h>
 #include <libdevcrypto/Common.h>
 #include <libdevcrypto/CryptoPP.h>
+
+using namespace std;
+using namespace dev;
+namespace js = json_spirit;
+
+void help()
+{
+	cout
+		<< "Usage bench <mode> [OPTIONS]" << endl
+		<< "Modes:" << endl
+		<< "    trie  Trie benchmarks." << endl
+		<< endl
+		<< "General options:" << endl
+		<< "    -h,--help  Print this help message and exit." << endl
+		<< "    -V,--version  Show the version and exit." << endl
+		;
+	exit(0);
+}
