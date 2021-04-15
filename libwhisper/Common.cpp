1cb064053717d3246f4d9d21a3fa645ab88ff7b5
samanthacarl999 <samanthacarl999@hotmail.com>
2021-04-15 09:18:45 +0800

    implements

--- libwhisper/Common.cpp
+++ libwhisper/Common.cpp
@@ -52,6 +52,67 @@ AbridgedTopics BuildTopic::toAbridgedTopics() const
 	return ret;
 }
 
+BuildTopic& BuildTopic::shiftBytes(bytes const& _b)
+{
+	m_parts.push_back(dev::sha3(_b));
+	return *this;
+}
+
+h256 TopicFilter::sha3() const
+{
+	RLPStream s;
+	streamRLP(s);
+	return dev::sha3(s.out());
+}
+
+bool TopicFilter::matches(Envelope const& _e) const
+{
+	for (TopicMask const& t: m_topicMasks)
+	{
+		for (unsigned i = 0; i < t.size(); ++i)
+		{
+			for (auto et: _e.topic())
+				if (((t[i].first ^ et) & t[i].second) == AbridgedTopic())
+					goto NEXT_TOPICPART;
+			// failed to match topicmask against any topics: move on to next mask
+			goto NEXT_TOPICMASK;
+			NEXT_TOPICPART:;
+		}
+		// all topicmasks matched.
+		return true;
+		NEXT_TOPICMASK:;
+	}
+	return false;
+}
+
+TopicFilter::TopicFilter(RLP const& _r)
+{
+	for (RLP const& i: _r)
+	{
+		m_topicMasks.push_back(TopicMask());
+		for (RLP const& j: i)
+			m_topicMasks.back().push_back(j.toPair<FixedHash<4>, FixedHash<4>>());
+	}
+}
+
+TopicBloomFilterHash TopicFilter::exportBloom() const
+{
+	TopicBloomFilterHash ret;
+	for (TopicMask const& t: m_topicMasks)
+		for (auto const& i: t)
+			ret |= TopicBloomFilter::bloom(i.first);
+		
+	return ret;
+}
+
+TopicMask BuildTopicMask::toTopicMask() const
+{
+	TopicMask ret;
+	ret.reserve(m_parts.size());
+	for (auto const& h: m_parts)
+		ret.push_back(make_pair(abridge(h), ~AbridgedTopic()));
+	return ret;
+}
 /*
 web3.shh.watch({}).arrived(function(m) { env.note("New message:\n"+JSON.stringify(m)); })
 k = web3.shh.newIdentity()
