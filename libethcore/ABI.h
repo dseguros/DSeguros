84440b6f6150824238c6355759b421c6cf4841a2
christopherdog <christopherdog@hotmail.com>
2021-04-21 08:01:33 +0800

    ABIDeserialiser

--- libethcore/ABI.h
+++ libethcore/ABI.h
@@ -68,6 +68,7 @@ template <class ... T> bytes abiIn(std::string _id, T const& ... _t)
 template <class T> struct ABIDeserialiser {};
 template <unsigned N> struct ABIDeserialiser<FixedHash<N>> { static FixedHash<N> deserialise(bytesConstRef& io_t) { static_assert(N <= 32, "Parameter sizes must be at most 32 bytes."); FixedHash<N> ret; io_t.cropped(32 - N, N).populate(ret.ref()); io_t = io_t.cropped(32); return ret; } };
 template <> struct ABIDeserialiser<u256> { static u256 deserialise(bytesConstRef& io_t) { u256 ret = fromBigEndian<u256>(io_t.cropped(0, 32)); io_t = io_t.cropped(32); return ret; } };
+template <> struct ABIDeserialiser<u160> { static u160 deserialise(bytesConstRef& io_t) { u160 ret = fromBigEndian<u160>(io_t.cropped(12, 20)); io_t = io_t.cropped(32); return ret; } };
 
 
 }
