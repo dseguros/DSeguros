29456fd77c7453ebc2504f8f46e498d969facacc
samanthacarl999 <samanthacarl999@hotmail.com>
2021-04-22 08:52:00 +0800

    update

--- libethcore/BasicAuthority.h
+++ libethcore/BasicAuthority.h
@@ -37,8 +37,8 @@ public:
 	unsigned revision() const override { return 0; }
 	unsigned sealFields() const override { return 1; }
 	bytes sealRLP() const override { return rlp(Signature()); }
-   
-   	void populateFromParent(BlockHeader&, BlockHeader const&) const override;
+
+	void populateFromParent(BlockHeader&, BlockHeader const&) const override;
 	StringHashMap jsInfo(BlockHeader const& _bi) const override;
 	void verify(Strictness _s, BlockHeader const& _bi, BlockHeader const& _parent, bytesConstRef _block) const override;
 	bool shouldSeal(Interface*) override;
@@ -48,16 +48,13 @@ public:
 	static BlockHeader& setSig(BlockHeader& _bi, Signature const& _sig) { _bi.setSeal(_sig); return _bi; }
 	void setSecret(Secret const& _s) { m_secret = _s; }
 	static void init();
+
 private:
 	bool onOptionChanging(std::string const& _name, bytes const& _value) override;
 
 	Secret m_secret;
 	AddressHash m_authorities;
 };
-
-
-}
-
 
 }
 }
