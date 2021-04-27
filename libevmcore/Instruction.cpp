c8116558bfd522b35368d8b162af5244b7067e68
samanthacarl999 <samanthacarl999@hotmail.com>
2021-04-27 09:10:40 +0800

    add Instruction.cpp file

---
+++ libevmcore/Instruction.cpp
@@ -0,0 +1,9 @@
+#include "Instruction.h"
+
+#include <functional>
+#include <libdevcore/Common.h>
+#include <libdevcore/CommonIO.h>
+#include <libdevcore/Log.h>
+using namespace std;
+using namespace dev;
+using namespace dev::eth;
