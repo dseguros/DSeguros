09155e90a297034753f6aec4c0d4ebb01e53038f
christopherdog <christopherdog@hotmail.com>
2021-04-28 08:45:23 +0800

    streamRLPFields

--- libethcore/BlockHeader.cpp
+++ libethcore/BlockHeader.cpp
@@ -50,3 +50,9 @@ h256 BlockHeader::hash(IncludeSeal _i) const
 	}
 	return memo;
 }
+
+void BlockHeader::streamRLPFields(RLPStream& _s) const
+{
+	_s	<< m_parentHash << m_sha3Uncles << m_author << m_stateRoot << m_transactionsRoot << m_receiptsRoot << m_logBloom
+		<< m_difficulty << m_number << m_gasLimit << m_gasUsed << m_timestamp << m_extraData;
+}
