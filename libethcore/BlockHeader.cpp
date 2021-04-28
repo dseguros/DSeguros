e79df56d4f52055c33398d228519ea63a76e503e
christopherdog <christopherdog@hotmail.com>
2021-04-28 08:46:53 +0800

    verify

--- libethcore/BlockHeader.cpp
+++ libethcore/BlockHeader.cpp
@@ -130,3 +130,65 @@ void BlockHeader::populateFromParent(BlockHeader const& _parent)
 	m_difficulty = 1;
 	m_gasUsed = 0;
 }
+
+void BlockHeader::verify(Strictness _s, BlockHeader const& _parent, bytesConstRef _block) const
+{
+	if (m_number > ~(unsigned)0)
+		BOOST_THROW_EXCEPTION(InvalidNumber());
+
+	if (_s != CheckNothingNew && m_gasUsed > m_gasLimit)
+		BOOST_THROW_EXCEPTION(TooMuchGasUsed() << RequirementError(bigint(m_gasLimit), bigint(m_gasUsed)));
+
+	if (_parent)
+	{
+		if (m_parentHash && _parent.hash() != m_parentHash)
+			BOOST_THROW_EXCEPTION(InvalidParentHash());
+
+		if (m_timestamp <= _parent.m_timestamp)
+			BOOST_THROW_EXCEPTION(InvalidTimestamp());
+
+		if (m_number != _parent.m_number + 1)
+			BOOST_THROW_EXCEPTION(InvalidNumber());
+	}
+
+	if (_block)
+	{
+		RLP root(_block);
+
+		auto txList = root[1];
+		auto expectedRoot = trieRootOver(txList.itemCount(), [&](unsigned i){ return rlp(i); }, [&](unsigned i){ return txList[i].data().toBytes(); });
+
+		clog(BlockInfoDiagnosticsChannel) << "Expected trie root:" << toString(expectedRoot);
+		if (m_transactionsRoot != expectedRoot)
+		{
+			MemoryDB tm;
+			GenericTrieDB<MemoryDB> transactionsTrie(&tm);
+			transactionsTrie.init();
+
+			vector<bytesConstRef> txs;
+
+			for (unsigned i = 0; i < txList.itemCount(); ++i)
+			{
+				RLPStream k;
+				k << i;
+
+				transactionsTrie.insert(&k.out(), txList[i].data());
+
+				txs.push_back(txList[i].data());
+				cdebug << toHex(k.out()) << toHex(txList[i].data());
+			}
+			cdebug << "trieRootOver" << expectedRoot;
+			cdebug << "orderedTrieRoot" << orderedTrieRoot(txs);
+			cdebug << "TrieDB" << transactionsTrie.root();
+			cdebug << "Contents:";
+			for (auto const& t: txs)
+				cdebug << toHex(t);
+
+			BOOST_THROW_EXCEPTION(InvalidTransactionsRoot() << Hash256RequirementError(expectedRoot, m_transactionsRoot));
+		}
+		clog(BlockInfoDiagnosticsChannel) << "Expected uncle hash:" << toString(sha3(root[2].data()));
+		if (m_sha3Uncles != sha3(root[2].data()))
+			BOOST_THROW_EXCEPTION(InvalidUnclesHash() << Hash256RequirementError(sha3(root[2].data()), m_sha3Uncles));
+	}
+}
+
