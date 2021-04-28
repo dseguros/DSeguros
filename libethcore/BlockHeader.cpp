dc99a1ffbf735a2af5b23e29968b4627dfdfe1f5
christopherdog <christopherdog@hotmail.com>
2021-04-28 08:44:30 +0800

    add blockheader.cpp file

---
+++ libethcore/BlockHeader.cpp
@@ -0,0 +1,13 @@
+#include <libdevcore/Common.h>
+#include <libdevcore/Log.h>
+#include <libdevcore/RLP.h>
+#include <libdevcore/TrieDB.h>
+#include <libdevcore/TrieHash.h>
+#include <libethcore/Common.h>
+#include "Exceptions.h"
+#include "BlockHeader.h"
+
+BlockHeader::BlockHeader()
+{
+}
+
