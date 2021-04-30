eedfe0659afe20ab844020116f9679c8a8c95165
christopherdog <christopherdog@hotmail.com>
2021-04-30 09:00:46 +0800

    PrecompiledContract

--- libethcore/ChainOperationParams.h
+++ libethcore/ChainOperationParams.h
@@ -11,6 +11,20 @@ namespace dev
 namespace eth
 {
 
+class PrecompiledContract
+{
+public:
+	PrecompiledContract() = default;
+	PrecompiledContract(
+		PrecompiledPricer const& _cost,
+		PrecompiledExecutor const& _exec,
+		u256 const& _startingBlock = 0
+	):
+		m_cost(_cost),
+		m_execute(_exec),
+		m_startingBlock(_startingBlock)
+	{}
+}
 
 
 }
