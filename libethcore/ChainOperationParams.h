f6c6f03131f119b1b27321e8fb3e0bf623bc86b6
christopherdog <christopherdog@hotmail.com>
2021-04-30 09:00:20 +0800

    chain operation params

---
+++ libethcore/ChainOperationParams.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <libdevcore/Common.h>
+#include "Common.h"
+#include <libethcore/Precompiled.h>
+
+#include <libevmcore/EVMSchedule.h>
+
+namespace dev
+{
+namespace eth
+{
+
+
+
+}
+}
