f01038881cc09bfda71a132ed5c5d08688fb6b96
samanthacarl999 <samanthacarl999@hotmail.com>
2021-05-07 08:33:33 +0800

    some define

--- libdevcore/Assertions.h
+++ libdevcore/Assertions.h
@@ -6,5 +6,15 @@
 namespace dev
 {
 
+#if defined(_MSC_VER)
+#define ETH_FUNC __FUNCSIG__
+#elif defined(__GNUC__)
+#define ETH_FUNC __PRETTY_FUNCTION__
+#else
+#define ETH_FUNC __func__
+#endif
+
+#define asserts(A) ::dev::assertAux(A, #A, __LINE__, __FILE__, ETH_FUNC)
+#define assertsEqual(A, B) ::dev::assertEqualAux(A, B, #A, #B, __LINE__, __FILE__, ETH_FUNC)
 
 }
