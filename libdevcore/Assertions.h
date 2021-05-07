9ee629aa56a4d9de2d5e3e5d44553f87ca2126ba
samanthacarl999 <samanthacarl999@hotmail.com>
2021-05-07 08:33:45 +0800

    assertAux

--- libdevcore/Assertions.h
+++ libdevcore/Assertions.h
@@ -17,4 +17,16 @@ namespace dev
 #define asserts(A) ::dev::assertAux(A, #A, __LINE__, __FILE__, ETH_FUNC)
 #define assertsEqual(A, B) ::dev::assertEqualAux(A, B, #A, #B, __LINE__, __FILE__, ETH_FUNC)
 
+inline bool assertAux(bool _a, char const* _aStr, unsigned _line, char const* _file, char const* _func)
+{
+	bool ret = _a;
+	if (!ret)
+	{
+		std::cerr << "Assertion failed:" << _aStr << " [func=" << _func << ", line=" << _line << ", file=" << _file << "]" << std::endl;
+#if ETH_DEBUG
+		debug_break();
+#endif
+	}
+	return !ret;
+}
 }
