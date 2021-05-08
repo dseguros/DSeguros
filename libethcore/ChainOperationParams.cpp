ba3420977914d10cb3872ed18f6b43deb65fd1fe
christopherdog <christopherdog@hotmail.com>
2021-05-08 08:35:08 +0800

    PrecompiledContract

--- libethcore/ChainOperationParams.cpp
+++ libethcore/ChainOperationParams.cpp
@@ -4,3 +4,18 @@
 using namespace std;
 using namespace dev;
 using namespace eth;
+
+PrecompiledContract::PrecompiledContract(
+	unsigned _base,
+	unsigned _word,
+	PrecompiledExecutor const& _exec,
+	u256 const& _startingBlock
+):
+	PrecompiledContract([=](bytesConstRef _in) -> bigint
+	{
+		bigint s = _in.size();
+		bigint b = _base;
+		bigint w = _word;
+		return b + (s + 31) / 32 * w;
+	}, _exec, _startingBlock)
+{}
