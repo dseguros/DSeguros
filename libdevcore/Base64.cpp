a9488c1bbeda1886fd331943e886976c755c9cfe
samanthacarl999 <samanthacarl999@hotmail.com>
2021-05-09 08:31:21 +0800

    fromBase64

--- libdevcore/Base64.cpp
+++ libdevcore/Base64.cpp
@@ -98,3 +98,50 @@ string dev::toBase64(bytesConstRef _in)
 
 	return ret;
 }
+
+bytes dev::fromBase64(string const& encoded_string)
+{
+	auto in_len = encoded_string.size();
+	int i = 0;
+	int j = 0;
+	int in_ = 0;
+	byte char_array_3[3];
+	byte char_array_4[4];
+	bytes ret;
+
+	while (in_len-- && encoded_string[in_] != '=' && is_base64(encoded_string[in_]))
+	{
+		char_array_4[i++] = encoded_string[in_]; in_++;
+		if (i == 4)
+		{
+			for (i = 0; i < 4; i++)
+				char_array_4[i] = find_base64_char_index(char_array_4[i]);
+
+			char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+			char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+			char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+			for (i = 0; (i < 3); i++)
+				ret.push_back(char_array_3[i]);
+			i = 0;
+		}
+	}
+
+	if (i)
+	{
+		for (j = i; j < 4; j++)
+			char_array_4[j] = 0;
+
+		for (j = 0; j < 4; j++)
+		char_array_4[j] = find_base64_char_index(char_array_4[j]);
+
+		char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+		char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+		char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+		for (j = 0; j < i - 1; j++)
+			ret.push_back(char_array_3[j]);
+	}
+
+	return ret;
+}
