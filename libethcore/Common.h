fd063633a676f0247ffecd2910c66bf61a8a3f1e
christopherdog <christopherdog@hotmail.com>
2021-05-10 08:59:50 +0800

    toAddress

--- libethcore/Common.h
+++ libethcore/Common.h
@@ -20,5 +20,17 @@ extern const unsigned c_minorProtocolVersion;
 
 /// Current database version.
 extern const unsigned c_databaseVersion;
+
+/// User-friendly string representation of the amount _b in wei.
+std::string formatBalance(bigint const& _b);
+
+DEV_SIMPLE_EXCEPTION(InvalidAddress);
+
+/// Convert the given string into an address.
+Address toAddress(std::string const& _s);
+
+/// Get information concerning the currency denominations.
+std::vector<std::pair<u256, std::string>> const& units();
+
 }
 }
