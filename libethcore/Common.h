4d0b15e3353ee4e900aa3f234ae9a7d3f635a147
christopherdog <christopherdog@hotmail.com>
2021-05-10 09:00:07 +0800

    define unit

--- libethcore/Common.h
+++ libethcore/Common.h
@@ -32,5 +32,20 @@ Address toAddress(std::string const& _s);
 /// Get information concerning the currency denominations.
 std::vector<std::pair<u256, std::string>> const& units();
 
+/// The log bloom's size (2048-bit).
+using LogBloom = h2048;
+
+/// Many log blooms.
+using LogBlooms = std::vector<LogBloom>;
+
+// The various denominations; here for ease of use where needed within code.
+static const u256 ether = exp10<18>();
+static const u256 finney = exp10<15>();
+static const u256 szabo = exp10<12>();
+static const u256 shannon = exp10<9>();
+static const u256 wei = exp10<0>();
+
 }
+
+
 }
