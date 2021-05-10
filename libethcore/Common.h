d04186c4db90888bd72d03b46a4871983c506583
christopherdog <christopherdog@hotmail.com>
2021-05-10 08:59:13 +0800

    add common.h file

---
+++ libethcore/Common.h
@@ -0,0 +1,17 @@
+
+#pragma once
+
+#include <string>
+#include <functional>
+#include <libdevcore/Common.h>
+#include <libdevcore/FixedHash.h>
+#include <libdevcrypto/Common.h>
+
+namespace dev
+{
+namespace eth
+{
+
+
+}
+}
