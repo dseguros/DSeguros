78074abf174f0ed3efd4597832d5a3e2c34b0d41
samanthacarl999 <samanthacarl999@hotmail.com>
2021-05-11 09:06:16 +0800

    define

--- libdevcore/Common.h
+++ libdevcore/Common.h
@@ -29,3 +29,15 @@ fdef _M_IX86
 #pragma warning(pop)
 #pragma GCC diagnostic pop
 #include "vector_ref.h"
+
+// CryptoPP defines byte in the global namespace, so must we.
+using byte = uint8_t;
+
+// Quote a given token stream to turn it into a string.
+#define DEV_QUOTED_HELPER(s) #s
+#define DEV_QUOTED(s) DEV_QUOTED_HELPER(s)
+
+#define DEV_IGNORE_EXCEPTIONS(X) try { X; } catch (...) {}
+
+#define DEV_IF_THROWS(X) try{X;}catch(...)
+
