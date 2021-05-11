4182ed156d077ece8438d7e8985942bf83823dd0
samanthacarl999 <samanthacarl999@hotmail.com>
2021-05-11 09:05:59 +0800

    some define

--- libdevcore/Common.h
+++ libdevcore/Common.h
@@ -1,2 +1,31 @@
 #pragma once
+fdef _M_IX86
+#pragma warning(disable:4244)
+#endif
 
+#if _MSC_VER && _MSC_VER < 1900
+#define _ALLOW_KEYWORD_MACROS
+#define noexcept throw()
+#endif
+
+#ifdef __INTEL_COMPILER
+#pragma warning(disable:3682) //call through incomplete class
+#endif
+
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <functional>
+#include <string>
+#include <chrono>
+#include <sys/time.h>
+#pragma warning(push)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#include <boost/multiprecision/cpp_int.hpp>
+#pragma warning(pop)
+#pragma GCC diagnostic pop
+#include "vector_ref.h"
