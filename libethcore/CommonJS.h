bc2fed1ddde3c6878b5a06900a21c20482fd9c80
christopherdog <christopherdog@hotmail.com>
2021-05-14 08:48:01 +0800

    inline Secret jsToSecret

--- libethcore/CommonJS.h
+++ libethcore/CommonJS.h
@@ -11,5 +11,8 @@ namespace dev
 /// Leniently convert string to Public (h512). Accepts integers, "0x" prefixing, non-exact length.
 inline Public jsToPublic(std::string const& _s) { return jsToFixed<sizeof(dev::Public)>(_s); }
 
+/// Leniently convert string to Secret (h256). Accepts integers, "0x" prefixing, non-exact length.
+inline Secret jsToSecret(std::string const& _s) { h256 d = jsToFixed<sizeof(dev::Secret)>(_s); Secret ret(d); d.ref().cleanse(); return ret; }
+
 }
 
