7049841b0df8eac4cd31135f431376ac15e993ba
christopherdog <christopherdog@hotmail.com>
2021-05-14 08:47:45 +0800

    namespace dev

--- libethcore/CommonJS.h
+++ libethcore/CommonJS.h
@@ -5,3 +5,11 @@
 #include <libdevcrypto/Common.h>
 #include "Common.h"
 
+namespace dev
+{
+
+/// Leniently convert string to Public (h512). Accepts integers, "0x" prefixing, non-exact length.
+inline Public jsToPublic(std::string const& _s) { return jsToFixed<sizeof(dev::Public)>(_s); }
+
+}
+
