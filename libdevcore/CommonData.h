543e255768bc8f5db8cfa6a2ffa64c659b17c08e
samanthacarl999 <samanthacarl999@hotmail.com>
2021-05-17 08:57:41 +0800

    std::string toHex

--- libdevcore/CommonData.h
+++ libdevcore/CommonData.h
@@ -25,6 +25,19 @@ enum class HexPrefix
 	Add = 1,
 };
 
+/// Convert a series of bytes to the corresponding string of hex duplets.
+/// @param _w specifies the width of the first of the elements. Defaults to two - enough to represent a byte.
+/// @example toHex("A\x69") == "4169"
+template <class T>
+std::string toHex(T const& _data, int _w = 2, HexPrefix _prefix = HexPrefix::DontAdd)
+{
+	std::ostringstream ret;
+	unsigned ii = 0;
+	for (auto i: _data)
+		ret << std::hex << std::setfill('0') << std::setw(ii++ ? 2 : _w) << (int)(typename std::make_unsigned<decltype(i)>::type)i;
+	return (_prefix == HexPrefix::Add) ? "0x" + ret.str() : ret.str();
+}
+
 }
 
 
