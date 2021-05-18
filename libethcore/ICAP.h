631eb7af80e7e100b889feaddf204be46b590203
christopherdog <christopherdog@hotmail.com>
2021-05-18 09:01:29 +0800

    class ICAP

--- libethcore/ICAP.h
+++ libethcore/ICAP.h
@@ -13,6 +13,24 @@ namespace dev
 namespace eth
 {
 
+DEV_SIMPLE_EXCEPTION(InvalidICAP);
+
+/**
+ * @brief Encapsulation of an ICAP address.
+ * Can be encoded, decoded, looked-up and inspected.
+ */
+class ICAP
+{
+public:
+	/// Construct null ICAP object.
+	ICAP() = default;
+	/// Construct a direct ICAP object for given target address. Must have a zero first byte.
+	ICAP(Address const& _target): m_type(Direct), m_direct(_target) {}
+	/// Construct an indirect ICAP object for given client and institution names.
+	ICAP(std::string const& _client, std::string const& _inst): m_type(Indirect), m_client(boost::algorithm::to_upper_copy(_client)), m_institution(boost::algorithm::to_upper_copy(_inst)), m_asset("XET") {}
+
+
+}
 
 }
 
