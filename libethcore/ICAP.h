6720b806aaff326839fec2749c12be86de35aa28
christopherdog <christopherdog@hotmail.com>
2021-05-18 08:53:04 +0800

    #include

--- libethcore/ICAP.h
+++ libethcore/ICAP.h
@@ -1,2 +1,10 @@
 #pragma once
 
+#include <string>
+#include <functional>
+#include <boost/algorithm/string/case_conv.hpp>
+#include <libdevcore/Common.h>
+#include <libdevcore/Exceptions.h>
+#include <libdevcore/FixedHash.h>
+#include "Common.h"
+
