d7e9e579c754066f605f66c4fc3f30c60836adc0
christopherdog <christopherdog@hotmail.com>
2021-05-20 08:51:54 +0800

    #include

--- libethcore/ICAP.cpp
+++ libethcore/ICAP.cpp
@@ -1,2 +1,12 @@
 #include "ICAP.h"
+#include <boost/algorithm/string/case_conv.hpp>
+#include <boost/algorithm/string.hpp>
+#include <libdevcore/Base64.h>
+#include <libdevcore/SHA3.h>
+#include "Exceptions.h"
+#include "ABI.h"
+using namespace std;
+using namespace dev;
+using namespace dev::eth;
+
 
