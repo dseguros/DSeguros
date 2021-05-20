12a7f723010a1722b52c3c70bcb736af3ec01804
christopherdog <christopherdog@hotmail.com>
2021-05-20 09:50:01 +0800

    ICAP::iban

--- libethcore/ICAP.cpp
+++ libethcore/ICAP.cpp
@@ -9,4 +9,27 @@ using namespace std;
 using namespace dev;
 using namespace dev::eth;
 
+namespace dev
+{
+namespace eth
+{
 
+string ICAP::iban(std::string _c, std::string _d)
+{
+	boost::to_upper(_c);
+	boost::to_upper(_d);
+	auto totStr = _d + _c + "00";
+	bigint tot = 0;
+	for (char x: totStr)
+		if (x >= 'A')
+			tot = tot * 100 + x - 'A' + 10;
+		else
+			tot = tot * 10 + x - '0';
+	unsigned check = (unsigned)(u256)(98 - tot % 97);
+	ostringstream out;
+	out << _c << setfill('0') << setw(2) << check << _d;
+	return out.str();
+}
+
+}
+}
