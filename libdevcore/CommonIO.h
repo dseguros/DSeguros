6454ee5d6500ead12de85bd58b9a5bd9faac0a57
samanthacarl999 <samanthacarl999@hotmail.com>
2021-05-21 05:20:01 +0800

    #include

--- libdevcore/CommonIO.h
+++ libdevcore/CommonIO.h
@@ -1,2 +1,18 @@
 #pragma once
 
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <array>
+#include <list>
+#include <memory>
+#include <vector>
+#include <array>
+#include <sstream>
+#include <string>
+#include <iostream>
+#include <chrono>
+#include "Common.h"
+#include "CommonData.h"
+#include "Base64.h"
