9043baf644b47077a237fa210d8c0a9f6223fcff
samanthacarl999 <samanthacarl999@hotmail.com>
2021-05-21 10:10:01 +0800

    template

--- libdevcore/CommonIO.h
+++ libdevcore/CommonIO.h
@@ -124,6 +124,127 @@ inline S& streamout(S& _out, std::array<T, Z> const& _e)
 }
 template <class T, std::size_t Z> inline std::ostream& operator<<(std::ostream& _out, std::array<T, Z> const& _e) { streamout(_out, _e); return _out; }
 
+template <class S, class T>
+inline S& streamout(S& _out, std::list<T> const& _e)
+{
+	_out << "[";
+	if (!_e.empty())
+	{
+		_out << _e.front();
+		for (auto i = ++_e.begin(); i != _e.end(); ++i)
+			_out << "," << *i;
+	}
+	_out << "]";
+	return _out;
+}
+template <class T> inline std::ostream& operator<<(std::ostream& _out, std::list<T> const& _e) { streamout(_out, _e); return _out; }
+
+template <class S, class T, class U>
+inline S& streamout(S& _out, std::pair<T, U> const& _e)
+{
+	_out << "(" << _e.first << "," << _e.second << ")";
+	return _out;
+}
+template <class T, class U> inline std::ostream& operator<<(std::ostream& _out, std::pair<T, U> const& _e) { streamout(_out, _e); return _out; }
+
+template <class S, class T1, class T2, class T3>
+inline S& streamout(S& _out, std::tuple<T1, T2, T3> const& _t)
+{
+	_out << "(" << std::get<0>(_t) << "," << std::get<1>(_t) << "," << std::get<2>(_t) << ")";
+	return _out;
+}
+template <class T1, class T2, class T3> inline std::ostream& operator<<(std::ostream& _out, std::tuple<T1, T2, T3> const& _e) { streamout(_out, _e); return _out; }
+
+template <class S, class T, class U>
+S& streamout(S& _out, std::map<T, U> const& _v)
+{
+	if (_v.empty())
+		return _out << "{}";
+	int i = 0;
+	for (auto p: _v)
+		_out << (!(i++) ? "{ " : "; ") << p.first << " => " << p.second;
+	return _out << " }";
+}
+template <class T, class U> inline std::ostream& operator<<(std::ostream& _out, std::map<T, U> const& _e) { streamout(_out, _e); return _out; }
+
+template <class S, class T, class U>
+S& streamout(S& _out, std::unordered_map<T, U> const& _v)
+{
+	if (_v.empty())
+		return _out << "{}";
+	int i = 0;
+	for (auto p: _v)
+		_out << (!(i++) ? "{ " : "; ") << p.first << " => " << p.second;
+	return _out << " }";
+}
+template <class T, class U> inline std::ostream& operator<<(std::ostream& _out, std::unordered_map<T, U> const& _e) { streamout(_out, _e); return _out; }
+
+template <class S, class T>
+S& streamout(S& _out, std::set<T> const& _v)
+{
+	if (_v.empty())
+		return _out << "{}";
+	int i = 0;
+	for (auto p: _v)
+		_out << (!(i++) ? "{ " : ", ") << p;
+	return _out << " }";
+}
+template <class T> inline std::ostream& operator<<(std::ostream& _out, std::set<T> const& _e) { streamout(_out, _e); return _out; }
+
+template <class S, class T>
+S& streamout(S& _out, std::unordered_set<T> const& _v)
+{
+	if (_v.empty())
+		return _out << "{}";
+	int i = 0;
+	for (auto p: _v)
+		_out << (!(i++) ? "{ " : ", ") << p;
+	return _out << " }";
+}
+template <class T> inline std::ostream& operator<<(std::ostream& _out, std::unordered_set<T> const& _e) { streamout(_out, _e); return _out; }
+
+template <class S, class T>
+S& streamout(S& _out, std::multiset<T> const& _v)
+{
+	if (_v.empty())
+		return _out << "{}";
+	int i = 0;
+	for (auto p: _v)
+		_out << (!(i++) ? "{ " : ", ") << p;
+	return _out << " }";
+}
+template <class T> inline std::ostream& operator<<(std::ostream& _out, std::multiset<T> const& _e) { streamout(_out, _e); return _out; }
+
+template <class S, class T, class U>
+S& streamout(S& _out, std::multimap<T, U> const& _v)
+{
+	if (_v.empty())
+		return _out << "{}";
+	T l;
+	int i = 0;
+	for (auto p: _v)
+		if (!(i++))
+			_out << "{ " << (l = p.first) << " => " << p.second;
+		else if (l == p.first)
+			_out << ", " << p.second;
+		else
+			_out << "; " << (l = p.first) << " => " << p.second;
+	return _out << " }";
+}
+template <class T, class U> inline std::ostream& operator<<(std::ostream& _out, std::multimap<T, U> const& _e) { streamout(_out, _e); return _out; }
+
+template <class _S, class _T> _S& operator<<(_S& _out, std::shared_ptr<_T> const& _p) { if (_p) _out << "@" << (*_p); else _out << "nullptr"; return _out; }
+
+// Functions that use streaming stuff.
+
+/// Converts arbitrary value to string representation using std::stringstream.
+template <class _T>
+std::string toString(_T const& _t)
+{
+	std::ostringstream o;
+	o << _t;
+	return o.str();
+}
 
 }
 
