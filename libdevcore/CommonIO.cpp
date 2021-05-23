900c347d8939433c39a3a1eca10b93f9b7f2a498
samanthacarl999 <samanthacarl999@hotmail.com>
2021-05-24 05:50:01 +0800

    logic

--- libdevcore/CommonIO.cpp
+++ libdevcore/CommonIO.cpp
@@ -16,5 +16,31 @@ using namespace dev;
 
 string dev::memDump(bytes const& _bytes, unsigned _width, bool _html)
 {
-
-};
+    stringstream ret;
+	if (_html)
+		ret << "<pre style=\"font-family: Monospace,Lucida Console,Courier,Courier New,sans-serif; font-size: small\">";
+	for (unsigned i = 0; i < _bytes.size(); i += _width)
+	{
+		ret << hex << setw(4) << setfill('0') << i << " ";
+		for (unsigned j = i; j < i + _width; ++j)
+			if (j < _bytes.size())
+				if (_bytes[j] >= 32 && _bytes[j] < 127)
+					if ((char)_bytes[j] == '<' && _html)
+						ret << "&lt;";
+					else if ((char)_bytes[j] == '&' && _html)
+						ret << "&amp;";
+					else
+						ret << (char)_bytes[j];
+				else
+					ret << '?';
+			else
+				ret << ' ';
+		ret << " ";
+		for (unsigned j = i; j < i + _width && j < _bytes.size(); ++j)
+			ret << setfill('0') << setw(2) << hex << (unsigned)_bytes[j] << " ";
+		ret << "\n";
+	}
+	if (_html)
+		ret << "</pre>";
+	return ret.str();
+}
