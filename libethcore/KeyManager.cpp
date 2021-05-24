b2acc4d4dc71e3f7a30d0bff25a47dd7dfe15d87
christopherdog <christopherdog@hotmail.com>
2021-05-25 07:50:01 +0800

    load

--- libethcore/KeyManager.cpp
+++ libethcore/KeyManager.cpp
@@ -64,3 +64,64 @@ bool KeyManager::recode(Address const& _address, SemanticPassword _newPass, func
 
 	return recode(_address, p, string(), _pass, _kdf);
 }
+
+bool KeyManager::load(string const& _pass)
+{
+	try
+	{
+		bytes salt = contents(m_keysFile + ".salt");
+		bytes encKeys = contents(m_keysFile);
+		if (encKeys.empty())
+			return false;
+		m_keysFileKey = SecureFixedHash<16>(pbkdf2(_pass, salt, 262144, 16));
+		bytesSec bs = decryptSymNoAuth(m_keysFileKey, h128(), &encKeys);
+		RLP s(bs.ref());
+		unsigned version = unsigned(s[0]);
+		if (version == 1)
+		{
+			bool saveRequired = false;
+			for (auto const& i: s[1])
+			{
+				h128 uuid(i[1]);
+				Address addr(i[0]);
+				if (uuid)
+				{
+					if (m_store.contains(uuid))
+					{
+						m_addrLookup[addr] = uuid;
+						m_uuidLookup[uuid] = addr;
+						m_keyInfo[addr] = KeyInfo(h256(i[2]), string(i[3]), i.itemCount() > 4 ? string(i[4]) : "");
+						if (m_store.noteAddress(uuid, addr))
+							saveRequired = true;
+					}
+					else
+						cwarn << "Missing key:" << uuid << addr;
+				}
+				else
+				{
+					// TODO: brain wallet.
+					m_keyInfo[addr] = KeyInfo(h256(i[2]), string(i[3]), i.itemCount() > 4 ? string(i[4]) : "");
+				}
+//				cdebug << toString(addr) << toString(uuid) << toString((h256)i[2]) << (string)i[3];
+			}
+			if (saveRequired)
+				m_store.save();
+
+			for (auto const& i: s[2])
+				m_passwordHint[h256(i[0])] = string(i[1]);
+			m_defaultPasswordDeprecated = string(s[3]);
+		}
+//		cdebug << hashPassword(m_password) << toHex(m_password);
+		cachePassword(m_defaultPasswordDeprecated);
+//		cdebug << hashPassword(asString(m_key.ref())) << m_key.hex();
+		cachePassword(asString(m_keysFileKey.ref()));
+//		cdebug << hashPassword(_pass) << _pass;
+		m_master = hashPassword(_pass);
+		cachePassword(_pass);
+		return true;
+	}
+	catch (...)
+	{
+		return false;
+	}
+}
