d090d3762d7865ed453008bddd9e91e74edd957b
samanthacarl999 <samanthacarl999@hotmail.com>
2021-05-24 09:20:01 +0800

    void dev::writeFile

--- libdevcore/CommonIO.cpp
+++ libdevcore/CommonIO.cpp
@@ -83,3 +83,31 @@ string dev::contentsString(string const& _file)
 {
 	return contentsGeneric<string>(_file);
 }
+
+void dev::writeFile(std::string const& _file, bytesConstRef _data, bool _writeDeleteRename)
+{
+	namespace fs = boost::filesystem;
+	if (_writeDeleteRename)
+	{
+		fs::path tempPath = fs::unique_path(_file + "-%%%%%%");
+		writeFile(tempPath.string(), _data, false);
+		// will delete _file if it exists
+		fs::rename(tempPath, _file);
+	}
+	else
+	{
+		// create directory if not existent
+		fs::path p(_file);
+		if (!fs::exists(p.parent_path()))
+		{
+			fs::create_directories(p.parent_path());
+			DEV_IGNORE_EXCEPTIONS(fs::permissions(p.parent_path(), fs::owner_all));
+		}
+
+		ofstream s(_file, ios::trunc | ios::binary);
+		s.write(reinterpret_cast<char const*>(_data.data()), _data.size());
+		if (!s)
+			BOOST_THROW_EXCEPTION(FileError() << errinfo_comment("Could not write to file: " + _file));
+		DEV_IGNORE_EXCEPTIONS(fs::permissions(_file, fs::owner_read|fs::owner_write));
+	}
+}
