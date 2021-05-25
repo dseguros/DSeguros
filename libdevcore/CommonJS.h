78be019568a0bf15513cd12d85d77cfe57e199a3
samanthacarl999 <samanthacarl999@hotmail.com>
2021-05-26 05:20:01 +0800

    toJS

--- libdevcore/CommonJS.h
+++ libdevcore/CommonJS.h
@@ -4,3 +4,14 @@
 #include "FixedHash.h"
 #include "CommonData.h"
 #include "CommonIO.h"
+
+namespace dev
+{
+
+template <unsigned S> std::string toJS(FixedHash<S> const& _h)
+{
+	return "0x" + toHex(_h.ref());
+}
+
+}
+
