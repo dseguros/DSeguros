e3c8904260b7f76a9a01cdc0dc78d202fdefe04b
samanthacarl999 <samanthacarl999@hotmail.com>
2021-05-26 05:50:01 +0800

    std::string toJS

--- libdevcore/CommonJS.h
+++ libdevcore/CommonJS.h
@@ -13,5 +13,13 @@ template <unsigned S> std::string toJS(FixedHash<S> const& _h)
 	return "0x" + toHex(_h.ref());
 }
 
+template <unsigned N> std::string toJS(boost::multiprecision::number<boost::multiprecision::cpp_int_backend<N, N, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>> const& _n)
+{
+	std::string h = toHex(toCompactBigEndian(_n, 1));
+	// remove first 0, if it is necessary;
+	std::string res = h[0] != '0' ? h : h.substr(1);
+	return "0x" + res;
+}
+
 }
 
