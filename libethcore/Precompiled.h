fb3b056a0e00c368147357feac7a509b80e313f3
christopherdog <christopherdog@hotmail.com>
2021-05-27 05:20:01 +0800

    PrecompiledExecutor

--- libethcore/Precompiled.h
+++ libethcore/Precompiled.h
@@ -5,3 +5,14 @@
 #include <libdevcore/CommonData.h>
 #include <libdevcore/Exceptions.h>
 
+namespace dev
+{
+namespace eth
+{
+
+using PrecompiledExecutor = std::function<std::pair<bool, bytes>(bytesConstRef _in)>;
+using PrecompiledPricer = std::function<bigint(bytesConstRef _in)>;
+
+}
+
+}
