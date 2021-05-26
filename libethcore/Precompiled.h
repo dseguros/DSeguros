697dec141dd1b6802712abb66fd7de274b8429ed
christopherdog <christopherdog@hotmail.com>
2021-05-27 07:20:01 +0800

    unregisterPricer

--- libethcore/Precompiled.h
+++ libethcore/Precompiled.h
@@ -24,6 +24,16 @@ public:
 
 	/// Get the price calculator object for @a _name function or @throw PricerNotFound if not found.
 	static PrecompiledPricer const& pricer(std::string const& _name);
+	
+	/// Register an executor. In general just use ETH_REGISTER_PRECOMPILED.
+	static PrecompiledExecutor registerExecutor(std::string const& _name, PrecompiledExecutor const& _exec) { return (get()->m_execs[_name] = _exec); }
+	/// Unregister an executor. Shouldn't generally be necessary.
+	static void unregisterExecutor(std::string const& _name) { get()->m_execs.erase(_name); }
+
+	/// Register a pricer. In general just use ETH_REGISTER_PRECOMPILED_PRICER.
+	static PrecompiledPricer registerPricer(std::string const& _name, PrecompiledPricer const& _exec) { return (get()->m_pricers[_name] = _exec); }
+	/// Unregister a pricer. Shouldn't generally be necessary.
+	static void unregisterPricer(std::string const& _name) { get()->m_pricers.erase(_name); }
 
 };
 }
