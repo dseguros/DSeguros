4d47d49f586de90a1d1c9c2218eca53ea29fc10f
samanthacarl999 <samanthacarl999@hotmail.com>
2021-05-28 07:50:01 +0800

    class concurrent_queue

--- libdevcore/concurrent_queue.h
+++ libdevcore/concurrent_queue.h
@@ -4,3 +4,16 @@
 #include <condition_variable>
 #include <mutex>
 
+namespace dev
+{
+
+/// Concurrent queue.
+/// You can push and pop elements to/from the queue. Pop will block until the queue is not empty.
+/// The default backend (_QueueT) is std::queue. It can be changed to any type that has
+/// proper push(), pop(), empty() and front() methods.
+template<typename _T, typename _QueueT = std::queue<_T>>
+class concurrent_queue
+{
+
+};
+}
