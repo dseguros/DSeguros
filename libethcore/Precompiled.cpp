da03de38095bdc3c1fc9c847592ccc9015a4ed45
christopherdog <christopherdog@hotmail.com>
2021-05-31 05:50:01 +0800

    precompiled cpp file

---
+++ libethcore/Precompiled.cpp
@@ -0,0 +1,7 @@
+#include "Precompiled.h"
+#include <libdevcore/Log.h>
+#include <libdevcore/SHA3.h>
+#include <libdevcrypto/Hash.h>
+#include <libdevcrypto/Common.h>
+#include <libethcore/Common.h>
+
