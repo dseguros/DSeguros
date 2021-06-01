1583f366309e10b65d1db18ef53cbebd21a04c93
christopherdog <christopherdog@hotmail.com>
2021-06-02 06:30:01 +0800

    setSealer

--- libethcore/SealEngine.h
+++ libethcore/SealEngine.h
@@ -30,6 +30,20 @@ public:
 	virtual bytes sealRLP() const { return bytes(); }
 	virtual StringHashMap jsInfo(BlockHeader const&) const { return StringHashMap(); }
 
+
+       /// Don't forget to call Super::verify when subclassing & overriding.
+	virtual void verify(Strictness _s, BlockHeader const& _bi, BlockHeader const& _parent = BlockHeader(), bytesConstRef _block = bytesConstRef()) const;
+	/// Additional verification for transactions in blocks.
+	virtual void verifyTransaction(ImportRequirements::value _ir, TransactionBase const& _t, EnvInfo const& _env) const;
+	/// Don't forget to call Super::populateFromParent when subclassing & overriding.
+	virtual void populateFromParent(BlockHeader& _bi, BlockHeader const& _parent) const;
+
+	bytes option(std::string const& _name) const { Guard l(x_options); return m_options.count(_name) ? m_options.at(_name) : bytes(); }
+	bool setOption(std::string const& _name, bytes const& _value) { Guard l(x_options); try { if (onOptionChanging(_name, _value)) { m_options[_name] = _value; return true; } } catch (...) {} return false; }
+
+	virtual strings sealers() const { return { "default" }; }
+	virtual std::string sealer() const { return "default"; }
+	virtual void setSealer(std::string const&) {}
 };
 
 }
