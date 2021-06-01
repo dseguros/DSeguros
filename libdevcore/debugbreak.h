4a1a6825c9a69abd56251d409619c2b6e7d7d0e5
samanthacarl999 <samanthacarl999@hotmail.com>
2021-06-01 09:00:01 +0800

    #if defined

--- libdevcore/debugbreak.h
+++ libdevcore/debugbreak.h
@@ -1,2 +1,13 @@
 #ifndef DEBUG_BREAK_H
 #define DEBUG_BREAK_H
+
+#if defined(_MSC_VER) || defined(__MINGW32__)
+
+#define debug_break __debugbreak
+
+#else
+
+#include <signal.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+
