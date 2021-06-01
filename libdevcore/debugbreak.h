8e32b31d46574ea1ac5fd0a8da094a4eb7bfc686
samanthacarl999 <samanthacarl999@hotmail.com>
2021-06-01 10:10:01 +0800

    #if defined

--- libdevcore/debugbreak.h
+++ libdevcore/debugbreak.h
@@ -20,3 +20,12 @@ enum {
 	 * Making __builtin_trap() unsuitable for breaking into the debugger */
 	DEBUG_BREAK_PREFER_BUILTIN_TRAP_TO_SIGTRAP = 0,
 };
+
+#if defined(__i386__) || defined(__x86_64__)
+enum { HAVE_TRAP_INSTRUCTION = 1, };
+__attribute__((gnu_inline, always_inline))
+static void __inline__ trap_instruction(void)
+{
+	__asm__ volatile("int $0x03");
+}
+#elif defined(__thumb__)
