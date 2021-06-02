f38eef6492e53cbfcf5d3a8401745c1c6a869867
samanthacarl999 <samanthacarl999@hotmail.com>
2021-06-03 06:30:01 +0800

    #include

--- libdevcore/Exceptions.h
+++ libdevcore/Exceptions.h
@@ -1,2 +1,13 @@
 #pragma once
 
+#include <exception>
+#include <string>
+#include <boost/exception/exception.hpp>
+#include <boost/exception/info.hpp>
+#include <boost/exception/info_tuple.hpp>
+#include <boost/exception/diagnostic_information.hpp>
+#include <boost/throw_exception.hpp>
+#include <boost/tuple/tuple.hpp>
+#include "CommonData.h"
+#include "FixedHash.h"
+
