d07889566112731ea68de24d79a711c577ba1457
samanthacarl999 <samanthacarl999@hotmail.com>
2021-06-03 07:20:01 +0800

    struct Exception

--- libdevcore/Exceptions.h
+++ libdevcore/Exceptions.h
@@ -11,3 +11,17 @@
 #include "CommonData.h"
 #include "FixedHash.h"
 
+namespace dev
+{
+
+/// Base class for all exceptions.
+struct Exception: virtual std::exception, virtual boost::exception
+{
+	Exception(std::string _message = std::string()): m_message(std::move(_message)) {}
+	const char* what() const noexcept override { return m_message.empty() ? std::exception::what() : m_message.c_str(); }
+
+private:
+	std::string m_message;
+};
+
+}
