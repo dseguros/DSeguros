1298d85aa31b357f4eb547c1808302007a9561dc
christopherdog <christopherdog@hotmail.com>
2021-06-04 06:30:01 +0800

    SealEngineRegistrar

--- libethcore/SealEngine.cpp
+++ libethcore/SealEngine.cpp
@@ -1,3 +1,10 @@
 #include "SealEngine.h"
 #include "Transaction.h"
 #include <libevm/ExtVMFace.h>
+
+using namespace std;
+using namespace dev;
+using namespace eth;
+
+SealEngineRegistrar* SealEngineRegistrar::s_this = nullptr;
+
