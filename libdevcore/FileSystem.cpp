75c20596b0595a705963c902ed409ad3213ccc08
samanthacarl999 <samanthacarl999@hotmail.com>
2021-06-05 05:50:01 +0800

    #include

--- libdevcore/FileSystem.cpp
+++ libdevcore/FileSystem.cpp
@@ -2,3 +2,15 @@
 #include "Common.h"
 #include "Log.h"
 
+#if defined(_WIN32)
+#include <shlobj.h>
+#else
+#include <stdlib.h>
+#include <stdio.h>
+#include <pwd.h>
+#include <unistd.h>
+#endif
+#include <boost/filesystem.hpp>
+using namespace std;
+using namespace dev;
+
