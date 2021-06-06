2430f8672532e26e2214833dd6e22fddc07a3cd4
christopherdog <christopherdog@hotmail.com>
2021-06-07 07:50:01 +0800

    forceSender

--- libethcore/Transaction.h
+++ libethcore/Transaction.h
@@ -47,6 +47,24 @@ public:
 	/// Constructs an unsigned contract-creation transaction.
 	TransactionBase(u256 const& _value, u256 const& _gasPrice, u256 const& _gas, bytes const& _data, u256 const& _nonce = 0): m_type(ContractCreation), m_nonce(_nonce), m_value(_value), m_gasPrice(_gasPrice), m_gas(_gas), m_data(_data) {}
 
+	/// Constructs a transaction from the given RLP.
+	explicit TransactionBase(bytesConstRef _rlp, CheckTransaction _checkSig);
+
+	/// Constructs a transaction from the given RLP.
+	explicit TransactionBase(bytes const& _rlp, CheckTransaction _checkSig): TransactionBase(&_rlp, _checkSig) {}
+
+	/// Checks equality of transactions.
+	bool operator==(TransactionBase const& _c) const { return m_type == _c.m_type && (m_type == ContractCreation || m_receiveAddress == _c.m_receiveAddress) && m_value == _c.m_value && m_data == _c.m_data; }
+	/// Checks inequality of transactions.
+	bool operator!=(TransactionBase const& _c) const { return !operator==(_c); }
+
+	/// @returns sender of the transaction from the signature (and hash).
+	Address const& sender() const;
+	/// Like sender() but will never throw. @returns a null Address if the signature is invalid.
+	Address const& safeSender() const noexcept;
+	/// Force the sender to a particular value. This will result in an invalid transaction RLP.
+	void forceSender(Address const& _a) { m_sender = _a; }
+
 };
 }
 }
