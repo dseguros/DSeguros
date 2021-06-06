fa1f74caf2c8edfb3acfb349861e37ea097db008
christopherdog <christopherdog@hotmail.com>
2021-06-07 05:50:01 +0800

    IncludeSignature

--- libethcore/Transaction.h
+++ libethcore/Transaction.h
@@ -4,3 +4,20 @@
 #include <libdevcore/SHA3.h>
 #include <libethcore/Common.h>
 
+namespace dev
+{
+namespace eth
+{
+
+struct EVMSchedule;
+
+/// Named-boolean type to encode whether a signature be included in the serialisation process.
+enum IncludeSignature
+{
+	WithoutSignature = 0,	///< Do not include a signature.
+	WithSignature = 1,		///< Do include a signature.
+};
+
+}
+}
+
