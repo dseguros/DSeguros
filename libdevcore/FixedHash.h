e5c536ea82174ee239a66ab7255394b6945eeff8
samanthacarl999 <samanthacarl999@hotmail.com>
2021-06-08 05:50:01 +0800

    class FixedHash

--- libdevcore/FixedHash.h
+++ libdevcore/FixedHash.h
@@ -7,3 +7,25 @@
 #include <boost/random/uniform_int_distribution.hpp>
 #include <boost/functional/hash.hpp>
 #include "CommonData.h"
+
+namespace dev
+{
+
+/// Compile-time calculation of Log2 of constant values.
+template <unsigned N> struct StaticLog2 { enum { result = 1 + StaticLog2<N/2>::result }; };
+template <> struct StaticLog2<1> { enum { result = 0 }; };
+
+extern boost::random_device s_fixedHashEngine;
+
+/// Fixed-size raw-byte array container type, with an API optimised for storing hashes.
+/// Transparently converts to/from the corresponding arithmetic type; this will
+/// assume the data contained in the hash is big-endian.
+template <unsigned N>
+class FixedHash
+{
+
+
+};
+
+}
+
