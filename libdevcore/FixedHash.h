a582df5ff4418f459042b5edc5fa5aa5b77a2a51
samanthacarl999 <samanthacarl999@hotmail.com>
2021-06-08 05:20:01 +0800

    FixedHash.h added

---
+++ libdevcore/FixedHash.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <array>
+#include <cstdint>
+#include <algorithm>
+#include <boost/random/random_device.hpp>
+#include <boost/random/uniform_int_distribution.hpp>
+#include <boost/functional/hash.hpp>
+#include "CommonData.h"
