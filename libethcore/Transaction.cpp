c49744fbb47f25df0e2c2cd6b5d04baa3dc94d63
christopherdog <christopherdog@hotmail.com>
2021-06-09 05:50:01 +0800

    TransactionBase::TransactionBase

--- libethcore/Transaction.cpp
+++ libethcore/Transaction.cpp
@@ -25,3 +25,58 @@ TransactionBase::TransactionBase(TransactionSkeleton const& _ts, Secret const& _
 		sign(_s);
 }
 
+TransactionBase::TransactionBase(bytesConstRef _rlpData, CheckTransaction _checkSig)
+{
+	int field = 0;
+	RLP rlp(_rlpData);
+	try
+	{
+		if (!rlp.isList())
+			BOOST_THROW_EXCEPTION(InvalidTransactionFormat() << errinfo_comment("transaction RLP must be a list"));
+
+		m_nonce = rlp[field = 0].toInt<u256>();
+		m_gasPrice = rlp[field = 1].toInt<u256>();
+		m_gas = rlp[field = 2].toInt<u256>();
+		m_type = rlp[field = 3].isEmpty() ? ContractCreation : MessageCall;
+		m_receiveAddress = rlp[field = 3].isEmpty() ? Address() : rlp[field = 3].toHash<Address>(RLP::VeryStrict);
+		m_value = rlp[field = 4].toInt<u256>();
+
+		if (!rlp[field = 5].isData())
+			BOOST_THROW_EXCEPTION(InvalidTransactionFormat() << errinfo_comment("transaction data RLP must be an array"));
+
+		m_data = rlp[field = 5].toBytes();
+
+		byte v = rlp[field = 6].toInt<byte>();
+		h256 r = rlp[field = 7].toInt<u256>();
+		h256 s = rlp[field = 8].toInt<u256>();
+
+		m_vrs = SignatureStruct{ r, s, v };
+
+		if (hasZeroSignature())
+			m_chainId = m_vrs.v;
+		else
+		{
+			if (v > 36)
+				m_chainId = (v - 35) / 2;
+			else if (v == 27 || v == 28)
+				m_chainId = -4;
+			else
+				BOOST_THROW_EXCEPTION(InvalidSignature());
+			m_vrs.v = v - (m_chainId * 2 + 35);
+
+			if (_checkSig >= CheckTransaction::Cheap && !m_vrs.isValid())
+				BOOST_THROW_EXCEPTION(InvalidSignature());
+		}
+
+		if (_checkSig == CheckTransaction::Everything)
+			m_sender = sender();
+
+		if (rlp.itemCount() > 9)
+			BOOST_THROW_EXCEPTION(InvalidTransactionFormat() << errinfo_comment("to many fields in the transaction RLP"));
+	}
+	catch (Exception& _e)
+	{
+		_e << errinfo_name("invalid transaction format: " + toString(rlp) + " RLP: " + toHex(rlp.data()));
+		throw;
+	}
+}
