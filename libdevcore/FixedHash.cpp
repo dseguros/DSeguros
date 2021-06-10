d15136e03f54b8473fd69d4c0de2a0bd62cda32b
samanthacarl999 <samanthacarl999@hotmail.com>
2021-06-10 10:10:01 +0800

    fromUUID

--- libdevcore/FixedHash.cpp
+++ libdevcore/FixedHash.cpp
@@ -6,3 +6,16 @@ using namespace std;
 using namespace dev;
 
 boost::random_device dev::s_fixedHashEngine;
+
+h128 dev::fromUUID(std::string const& _uuid)
+{
+	try
+	{
+		return h128(boost::replace_all_copy(_uuid, "-", ""));
+	}
+	catch (...)
+	{
+		return h128();
+	}
+}
+
