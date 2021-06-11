21b8e95a116962988e1af6642e5fee26dc4f61b6
christopherdog <christopherdog@hotmail.com>
2021-06-11 09:00:01 +0800

    BasicGasPricer

--- libethereum/BasicGasPricer.h
+++ libethereum/BasicGasPricer.h
@@ -8,6 +8,12 @@ namespace dev
 namespace eth
 {
 
+class BasicGasPricer: public GasPricer
+{
+public:
+	explicit BasicGasPricer(u256 _weiPerRef, u256 _refsPerBlock): m_weiPerRef(_weiPerRef), m_refsPerBlock(_refsPerBlock) {}
+
+};
 }
 }
 
