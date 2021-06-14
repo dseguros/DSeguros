655cb2ed3c50cb9276cfdedc681df689ff95c9f1
samanthacarl999 <samanthacarl999@hotmail.com>
2021-06-15 05:20:01 +0800

    #include

--- libdevcore/Guards.h
+++ libdevcore/Guards.h
@@ -1,2 +1,12 @@
 #pragma once
 
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+#pragma warning(push)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#include <boost/thread.hpp>
+#pragma warning(pop)
+#pragma GCC diagnostic pop
+
