febbd7542d557bbac888608eb99ca3bcf71938e0
samanthacarl999 <samanthacarl999@hotmail.com>
2021-06-15 11:40:01 +0800

    #define

--- libdevcore/Guards.h
+++ libdevcore/Guards.h
@@ -87,4 +87,51 @@ private:
 	N m_value;
 };
 
+/** @brief Simple block guard.
+ * The expression/block following is guarded though the given mutex.
+ * Usage:
+ * @code
+ * Mutex m;
+ * unsigned d;
+ * ...
+ * ETH_(m) d = 1;
+ * ...
+ * ETH_(m) { for (auto d = 10; d > 0; --d) foo(d); d = 0; }
+ * @endcode
+ *
+ * There are several variants of this basic mechanism for different Mutex types and Guards.
+ *
+ * There is also the UNGUARD variant which allows an unguarded expression/block to exist within a
+ * guarded expression. eg:
+ *
+ * @code
+ * Mutex m;
+ * int d;
+ * ...
+ * ETH_GUARDED(m)
+ * {
+ *   for (auto d = 50; d > 25; --d)
+ *     foo(d);
+ *   ETH_UNGUARDED(m)
+ *     bar();
+ *   for (; d > 0; --d)
+ *     foo(d);
+ * }
+ * @endcode
+ */
+
+#define DEV_GUARDED(MUTEX) \
+	for (GenericGuardBool<Guard, Mutex> __eth_l(MUTEX); __eth_l.b; __eth_l.b = false)
+#define DEV_READ_GUARDED(MUTEX) \
+	for (GenericGuardBool<ReadGuard, SharedMutex> __eth_l(MUTEX); __eth_l.b; __eth_l.b = false)
+#define DEV_WRITE_GUARDED(MUTEX) \
+	for (GenericGuardBool<WriteGuard, SharedMutex> __eth_l(MUTEX); __eth_l.b; __eth_l.b = false)
+#define DEV_RECURSIVE_GUARDED(MUTEX) \
+	for (GenericGuardBool<RecursiveGuard, RecursiveMutex> __eth_l(MUTEX); __eth_l.b; __eth_l.b = false)
+#define DEV_UNGUARDED(MUTEX) \
+	for (GenericUnguardBool<Mutex> __eth_l(MUTEX); __eth_l.b; __eth_l.b = false)
+#define DEV_READ_UNGUARDED(MUTEX) \
+	for (GenericUnguardSharedBool<SharedMutex> __eth_l(MUTEX); __eth_l.b; __eth_l.b = false)
+#define DEV_WRITE_UNGUARDED(MUTEX) \
+	for (GenericUnguardBool<SharedMutex> __eth_l(MUTEX); __eth_l.b; __eth_l.b = false)
 }
