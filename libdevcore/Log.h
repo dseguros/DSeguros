8f297d483d9ac4954bc1932402e8f90befdb88de
samanthacarl999 <samanthacarl999@hotmail.com>
2021-06-17 05:20:01 +0800

    Log.h added

---
+++ libdevcore/Log.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <ctime>
+#include <chrono>
+#include "vector_ref.h"
+#include "Common.h"
+#include "CommonIO.h"
+#include "CommonData.h"
+#include "FixedHash.h"
+#include "Terminal.h"
