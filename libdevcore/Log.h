18d533bc404cf6bd58e6fee24dd5fe435f420a84
samanthacarl999 <samanthacarl999@hotmail.com>
2021-06-17 05:50:01 +0800

    class NullOutputStream

--- libdevcore/Log.h
+++ libdevcore/Log.h
@@ -8,3 +8,19 @@
 #include "CommonData.h"
 #include "FixedHash.h"
 #include "Terminal.h"
+
+namespace boost { namespace asio { namespace ip { template<class T>class basic_endpoint; class tcp; } } }
+
+namespace dev
+{
+
+/// The null output stream. Used when logging is disabled.
+class NullOutputStream
+{
+public:
+	template <class T> NullOutputStream& operator<<(T const&) { return *this; }
+};
+
+}
+}
+
