35430062124469a7d2bbc115879c516b33529949
samanthacarl999 <samanthacarl999@hotmail.com>
2021-06-17 09:20:01 +0800

    LogTag

--- libdevcore/Log.h
+++ libdevcore/Log.h
@@ -74,6 +74,52 @@ public:
 	static std::string join(std::string const& _prior);
 };
 
+/// Set the current thread's log name.
+///
+/// It appears that there is not currently any cross-platform way of setting
+/// thread names either in Boost or in the C++11 runtime libraries.   What is
+/// more, the API for 'pthread_setname_np' is not even consistent across
+/// platforms which implement it.
+///
+/// A proposal to add such functionality on the Boost mailing list, which
+/// I assume never happened, but which I should follow-up and ask about.
+/// http://boost.2283326.n4.nabble.com/Adding-an-option-to-set-the-name-of-a-boost-thread-td4638283.html
+///
+/// man page for 'pthread_setname_np', including this crucial snippet of
+/// information ... "These functions are nonstandard GNU extensions."
+/// http://man7.org/linux/man-pages/man3/pthread_setname_np.3.html
+///
+/// Stack Overflow "Can I set the name of a thread in pthreads / linux?"
+/// which includes useful information on the minor API differences between
+/// Linux, BSD and OS X.
+/// http://stackoverflow.com/questions/2369738/can-i-set-the-name-of-a-thread-in-pthreads-linux/7989973#7989973
+///
+/// musl mailng list posting "pthread set name on MIPs" which includes the
+/// information that musl doesn't currently implement 'pthread_setname_np'
+/// https://marc.info/?l=musl&m=146171729013062&w=1
+void setThreadName(std::string const& _n);
+
+/// Set the current thread's log name.
+std::string getThreadName();
+
+/// The default logging channels. Each has an associated verbosity and three-letter prefix (name() ).
+/// Channels should inherit from LogChannel and define name() and verbosity.
+struct LogChannel { static const char* name(); static const int verbosity = 1; static const bool debug = true; };
+struct LeftChannel: public LogChannel { static const char* name(); };
+struct RightChannel: public LogChannel { static const char* name(); };
+struct WarnChannel: public LogChannel { static const char* name(); static const int verbosity = 0; static const bool debug = false; };
+struct NoteChannel: public LogChannel { static const char* name(); static const bool debug = false; };
+struct DebugChannel: public LogChannel { static const char* name(); static const int verbosity = 0; };
+struct TraceChannel: public LogChannel { static const char* name(); static const int verbosity = 4; static const bool debug = true; };
+
+enum class LogTag
+{
+	None,
+	Url,
+	Error,
+	Special
+};
+
 }
 }
 
