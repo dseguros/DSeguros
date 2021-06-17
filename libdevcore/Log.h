5d03f2c0870f1883fa616eb4556e28705d400f78
samanthacarl999 <samanthacarl999@hotmail.com>
2021-06-17 10:10:01 +0800

    LogOutputStreamBase

--- libdevcore/Log.h
+++ libdevcore/Log.h
@@ -120,6 +120,117 @@ enum class LogTag
 	Special
 };
 
+class LogOutputStreamBase
+{
+public:
+	LogOutputStreamBase(char const* _id, std::type_info const* _info, unsigned _v, bool _autospacing);
+
+	void comment(std::string const& _t)
+	{
+		switch (m_logTag)
+		{
+		case LogTag::Url: m_sstr << EthNavyUnder; break;
+		case LogTag::Error: m_sstr << EthRedBold; break;
+		case LogTag::Special: m_sstr << EthWhiteBold; break;
+		default:;
+		}
+		m_sstr << _t << EthReset;
+		m_logTag = LogTag::None;
+	}
+
+	void append(unsigned long _t) { m_sstr << EthBlue << _t << EthReset; }
+	void append(long _t) { m_sstr << EthBlue << _t << EthReset; }
+	void append(unsigned int _t) { m_sstr << EthBlue << _t << EthReset; }
+	void append(int _t) { m_sstr << EthBlue << _t << EthReset; }
+	void append(bigint const& _t) { m_sstr << EthNavy << _t << EthReset; }
+	void append(u256 const& _t) { m_sstr << EthNavy << _t << EthReset; }
+	void append(u160 const& _t) { m_sstr << EthNavy << _t << EthReset; }
+	void append(double _t) { m_sstr << EthBlue << _t << EthReset; }
+	template <unsigned N> void append(FixedHash<N> const& _t) { m_sstr << EthTeal "#" << _t.abridged() << EthReset; }
+	void append(h160 const& _t) { m_sstr << EthRed "@" << _t.abridged() << EthReset; }
+	void append(h256 const& _t) { m_sstr << EthCyan "#" << _t.abridged() << EthReset; }
+	void append(h512 const& _t) { m_sstr << EthTeal "##" << _t.abridged() << EthReset; }
+	void append(std::string const& _t) { m_sstr << EthGreen "\"" + _t + "\"" EthReset; }
+	void append(bytes const& _t) { m_sstr << EthYellow "%" << toHex(_t) << EthReset; }
+	void append(bytesConstRef _t) { m_sstr << EthYellow "%" << toHex(_t) << EthReset; }
+	template <class T> void append(std::vector<T> const& _t)
+	{
+		m_sstr << EthWhite "[" EthReset;
+		int n = 0;
+		for (auto const& i: _t)
+		{
+			m_sstr << (n++ ? EthWhite ", " EthReset : "");
+			append(i);
+		}
+		m_sstr << EthWhite "]" EthReset;
+	}
+	template <class T> void append(std::set<T> const& _t)
+	{
+		m_sstr << EthYellow "{" EthReset;
+		int n = 0;
+		for (auto const& i: _t)
+		{
+			m_sstr << (n++ ? EthYellow ", " EthReset : "");
+			append(i);
+		}
+		m_sstr << EthYellow "}" EthReset;
+	}
+	template <class T, class U> void append(std::map<T, U> const& _t)
+	{
+		m_sstr << EthLime "{" EthReset;
+		int n = 0;
+		for (auto const& i: _t)
+		{
+			m_sstr << (n++ ? EthLime ", " EthReset : "");
+			append(i.first);
+			m_sstr << (n++ ? EthLime ": " EthReset : "");
+			append(i.second);
+		}
+		m_sstr << EthLime "}" EthReset;
+	}
+	template <class T> void append(std::unordered_set<T> const& _t)
+	{
+		m_sstr << EthYellow "{" EthReset;
+		int n = 0;
+		for (auto const& i: _t)
+		{
+			m_sstr << (n++ ? EthYellow ", " EthReset : "");
+			append(i);
+		}
+		m_sstr << EthYellow "}" EthReset;
+	}
+	template <class T, class U> void append(std::unordered_map<T, U> const& _t)
+	{
+		m_sstr << EthLime "{" EthReset;
+		int n = 0;
+		for (auto const& i: _t)
+		{
+			m_sstr << (n++ ? EthLime ", " EthReset : "");
+			append(i.first);
+			m_sstr << (n++ ? EthLime ": " EthReset : "");
+			append(i.second);
+		}
+		m_sstr << EthLime "}" EthReset;
+	}
+	template <class T, class U> void append(std::pair<T, U> const& _t)
+	{
+		m_sstr << EthPurple "(" EthReset;
+		append(_t.first);
+		m_sstr << EthPurple ", " EthReset;
+		append(_t.second);
+		m_sstr << EthPurple ")" EthReset;
+	}
+	template <class T> void append(T const& _t)
+	{
+		m_sstr << toString(_t);
+	}
+
+protected:
+	bool m_autospacing = false;
+	unsigned m_verbosity = 0;
+	std::stringstream m_sstr;	///< The accrued log entry.
+	LogTag m_logTag = LogTag::None;
+};
 }
 }
 
