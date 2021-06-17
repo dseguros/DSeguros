1d8d57af9a4ba4bd2a479309941c58fe18ea62ef
christopherdog <christopherdog@hotmail.com>
2021-06-18 06:30:01 +0800

    Account.h added

---
+++ libethereum/Account.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <libdevcore/Common.h>
+#include <libdevcore/RLP.h>
+#include <libdevcore/TrieDB.h>
+#include <libdevcore/SHA3.h>
+#include <libethcore/Common.h>
+
+namespace dev
+{
+namespace eth
+{
+
+}
+}
