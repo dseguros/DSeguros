3ccd152b4fbfbd5b63ba19fb53123c1136983079
samanthacarl999 <samanthacarl999@hotmail.com>
2021-06-19 05:20:01 +0800

    s_logOverride

--- libdevcore/Log.cpp
+++ libdevcore/Log.cpp
@@ -1,2 +1,24 @@
 #include "Log.h"
 
+#include <string>
+#include <iostream>
+#include <thread>
+#ifdef __APPLE__
+#include <pthread.h>
+#endif
+#include "Guards.h"
+using namespace std;
+using namespace dev;
+
+
+// Logging
+int dev::g_logVerbosity = 5;
+mutex x_logOverride;
+
+/// Map of Log Channel types to bool, false forces the channel to be disabled, true forces it to be enabled.
+/// If a channel has no entry, then it will output as long as its verbosity (LogChannel::verbosity) is less than
+/// or equal to the currently output verbosity (g_logVerbosity).
+static map<type_info const*, bool> s_logOverride;
+
+
+
