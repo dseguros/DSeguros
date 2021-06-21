d8649a958305593d6572f5311a5a55b07ca28760
christopherdog <christopherdog@hotmail.com>
2021-06-21 10:10:01 +0800

    createPrecompiledContract

--- libethereum/Account.cpp
+++ libethereum/Account.cpp
@@ -25,5 +25,34 @@ uint64_t toUnsigned(js::mValue const& _v)
 	}
 }
 
+PrecompiledContract createPrecompiledContract(js::mObject& _precompiled)
+{
+	auto n = _precompiled["name"].get_str();
+	try
+	{
+		u256 startingBlock = 0;
+		if (_precompiled.count("startingBlock"))
+			startingBlock = u256(_precompiled["startingBlock"].get_str());
+
+		if (!_precompiled.count("linear"))
+			return PrecompiledContract(PrecompiledRegistrar::pricer(n), PrecompiledRegistrar::executor(n), startingBlock);
+
+		auto l = _precompiled["linear"].get_obj();
+		unsigned base = toUnsigned(l["base"]);
+		unsigned word = toUnsigned(l["word"]);
+		return PrecompiledContract(base, word, PrecompiledRegistrar::executor(n), startingBlock);
+	}
+	catch (PricerNotFound const&)
+	{
+		cwarn << "Couldn't create a precompiled contract account. Missing a pricer called:" << n;
+		throw;
+	}
+	catch (ExecutorNotFound const&)
+	{
+		// Oh dear - missing a plugin?
+		cwarn << "Couldn't create a precompiled contract account. Missing an executor called:" << n;
+		throw;
+	}
+}
 }
 
