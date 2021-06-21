1a5c4dc2968bf6af16e2166976578e520b72bb8a
christopherdog <christopherdog@hotmail.com>
2021-06-21 09:00:01 +0800

    setNewCode

--- libethereum/Account.cpp
+++ libethereum/Account.cpp
@@ -3,3 +3,11 @@
 #include <libethcore/ChainOperationParams.h>
 #include <libethcore/Precompiled.h>
 
+void Account::setNewCode(bytes&& _code)
+{
+	m_codeCache = std::move(_code);
+	m_hasNewCode = true;
+	m_codeHash = sha3(m_codeCache);
+}
+
+
