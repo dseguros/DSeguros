27a7bb45892a4fcc2be10af306b78c9b033f451e
samanthacarl999 <samanthacarl999@hotmail.com>
2021-06-23 05:50:01 +0800

    MemoryDB.h added

---
+++ libdevcore/MemoryDB.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <unordered_map>
+#include "Common.h"
+#include "Guards.h"
+#include "FixedHash.h"
+#include "Log.h"
+#include "RLP.h"
+#include "SHA3.h"
+
+namespace dev
+{
+
+
+}
