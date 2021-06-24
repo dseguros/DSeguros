946b157da73d11d1694f0909142ee7b062a6bd9f
christopherdog <christopherdog@hotmail.com>
2021-06-24 09:15:38 +0800

    Block.h added

---
+++ libethereum/Block.h
@@ -0,0 +1,24 @@
+
+#pragma once
+
+#include <array>
+#include <unordered_map>
+#include <libdevcore/Common.h>
+#include <libdevcore/RLP.h>
+#include <libdevcore/TrieDB.h>
+#include <libdevcore/OverlayDB.h>
+#include <libethcore/Exceptions.h>
+#include <libethcore/BlockHeader.h>
+#include <libethcore/ChainOperationParams.h>
+#include <libevm/ExtVMFace.h>
+#include "Account.h"
+#include "Transaction.h"
+#include "TransactionReceipt.h"
+#include "GasPricer.h"
+#include "State.h"
+
+namespace dev
+{
+
+
+}
