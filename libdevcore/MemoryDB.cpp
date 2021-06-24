e4fbc7444c118926a000f7cadc76691c93c15a80
samanthacarl999 <samanthacarl999@hotmail.com>
2021-06-25 06:30:01 +0800

    MemoryDB::get()

--- libdevcore/MemoryDB.cpp
+++ libdevcore/MemoryDB.cpp
@@ -9,4 +9,17 @@ namespace dev
 const char* DBChannel::name() { return "TDB"; }
 const char* DBWarn::name() { return "TDB"; }
 
+
+std::unordered_map<h256, std::string> MemoryDB::get() const
+{
+#if DEV_GUARDED_DB
+	ReadGuard l(x_this);
+#endif
+	std::unordered_map<h256, std::string> ret;
+	for (auto const& i: m_main)
+		if (!m_enforceRefs || i.second.second > 0)
+			ret.insert(make_pair(i.first, i.second.first));
+	return ret;
+}
+
 }
