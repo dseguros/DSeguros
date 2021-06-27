c87315b0dc0ddd94402b26bcd2defe22ffa5ffea
christopherdog <christopherdog@hotmail.com>
2021-06-28 05:20:01 +0800

    Block.cpp added

---
+++ libethereum/Block.cpp
@@ -0,0 +1,44 @@
+#include "Block.h"
+
+#include <ctime>
+#include <boost/filesystem.hpp>
+#include <boost/timer.hpp>
+#include <libdevcore/CommonIO.h>
+#include <libdevcore/Assertions.h>
+#include <libdevcore/TrieHash.h>
+#include <libevmcore/Instruction.h>
+#include <libethcore/Exceptions.h>
+#include <libethcore/SealEngine.h>
+#include <libevm/VMFactory.h>
+#include "BlockChain.h"
+#include "Defaults.h"
+#include "ExtVM.h"
+#include "Executive.h"
+#include "BlockChain.h"
+#include "TransactionQueue.h"
+#include "GenesisInfo.h"
+using namespace std;
+using namespace dev;
+using namespace dev::eth;
+namespace fs = boost::filesystem;
+
+#define ETH_TIMED_ENACTMENTS 0
+
+static const unsigned c_maxSyncTransactions = 1024;
+
+const char* BlockSafeExceptions::name() { return EthViolet "⚙" EthBlue " ℹ"; }
+const char* BlockDetail::name() { return EthViolet "⚙" EthWhite " ◌"; }
+const char* BlockTrace::name() { return EthViolet "⚙" EthGray " ◎"; }
+const char* BlockChat::name() { return EthViolet "⚙" EthWhite " ◌"; }
+
+Block::Block(BlockChain const& _bc, OverlayDB const& _db, BaseState _bs, Address const& _author):
+	m_state(Invalid256, _db, _bs),
+	m_precommit(Invalid256),
+	m_author(_author)
+{
+	noteChain(_bc);
+	m_previousBlock.clear();
+	m_currentBlock.clear();
+//	assert(m_state.root() == m_previousBlock.stateRoot());
+}
+
