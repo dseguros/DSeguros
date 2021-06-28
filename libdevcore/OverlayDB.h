d183b19edbf349a97a2009ff3f7a545103873ce1
samanthacarl999 <samanthacarl999@hotmail.com>
2021-06-29 05:00:01 +0800

    class OverlayDB

--- libdevcore/OverlayDB.h
+++ libdevcore/OverlayDB.h
@@ -9,5 +9,24 @@
 namespace dev
 {
 
+class OverlayDB: public MemoryDB
+{
+public:
+	OverlayDB(ldb::DB* _db = nullptr): m_db(_db) {}
+	~OverlayDB();
+
+	ldb::DB* db() const { return m_db.get(); }
+
+	void commit();
+	void rollback();
+
+	std::string lookup(h256 const& _h) const;
+	bool exists(h256 const& _h) const;
+	void kill(h256 const& _h);
+	bool deepkill(h256 const& _h);
+
+	bytes lookupAux(h256 const& _h) const;
+
+};
 
 }
