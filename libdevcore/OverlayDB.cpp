ec2c0fe426700c6bb7c540fc86e5caab9f4a8cc7
samanthacarl999 <samanthacarl999@hotmail.com>
2021-06-29 05:50:01 +0800

    cpp file for OverlayDB

---
+++ libdevcore/OverlayDB.cpp
@@ -0,0 +1,16 @@
+#if !defined(ETH_EMSCRIPTEN)
+
+#include <thread>
+#include <libdevcore/db.h>
+#include <libdevcore/Common.h>
+#include "OverlayDB.h"
+using namespace std;
+using namespace dev;
+
+namespace dev
+{
+
+
+}
+
+#endif // ETH_EMSCRIPTEN
