98f190dd3f5958000d6e374760925fe1feecd77b
christopherdog <christopherdog@hotmail.com>
2021-06-30 06:30:01 +0800

    HasInvariants

--- libethereum/BlockChainSync.h
+++ libethereum/BlockChainSync.h
@@ -22,6 +22,29 @@ class EthereumHost;
 class BlockQueue;
 class EthereumPeer;
 
+/**
+ * @brief Base BlockChain synchronization strategy class.
+ * Syncs to peers and keeps up to date. Base class handles blocks downloading but does not contain any details on state transfer logic.
+ */
+class BlockChainSync: public HasInvariants
+{
+public:
+	BlockChainSync(EthereumHost& _host);
+	~BlockChainSync();
+	void abortSync(); ///< Abort all sync activity
+
+	/// @returns true is Sync is in progress
+	bool isSyncing() const;
+
+	/// Restart sync
+	void restartSync();
+
+	/// Called after all blocks have been downloaded
+	/// Public only for test mode
+	void completeSync();
+
+};
+
 }
 
 }
