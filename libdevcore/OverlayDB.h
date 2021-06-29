a052ef90ca01a122cd26036372500c2f220e1534
samanthacarl999 <samanthacarl999@hotmail.com>
2021-06-29 09:11:45 +0800

    OverlayDB.h added

---
+++ libdevcore/OverlayDB.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <memory>
+#include <libdevcore/db.h>
+#include <libdevcore/Common.h>
+#include <libdevcore/Log.h>
+#include <libdevcore/MemoryDB.h>
+
+namespace dev
+{
+
+
+}
