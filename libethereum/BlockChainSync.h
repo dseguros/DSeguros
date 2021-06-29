fa619e6035b07e0aea4813c96610e174c6c1ba4b
christopherdog <christopherdog@hotmail.com>
2021-06-30 05:20:01 +0800

    BlockChainSync.h added

---
+++ libethereum/BlockChainSync.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#include <mutex>
+#include <unordered_map>
+
+#include <libdevcore/Guards.h>
+#include <libethcore/Common.h>
+#include <libethcore/BlockHeader.h>
+#include <libp2p/Common.h>
+#include "CommonNet.h"
+
+namespace dev
+{
+
+}
