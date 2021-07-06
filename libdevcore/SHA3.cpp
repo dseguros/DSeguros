771e83a7af787d72f3852d3a8397a5af4d7149c3
samanthacarl999 <samanthacarl999@hotmail.com>
2021-07-07 07:50:01 +0800

    decsha3

--- libdevcore/SHA3.cpp
+++ libdevcore/SHA3.cpp
@@ -13,4 +13,30 @@ namespace dev
 h256 EmptySHA3 = sha3(bytesConstRef());
 h256 EmptyListSHA3 = sha3(rlpList());
 
+namespace keccak
+{
+
+/** libkeccak-tiny
+ *
+ * A single-file implementation of SHA-3 and SHAKE.
+ *
+ * Implementor: David Leon Gil
+ * License: CC0, attribution kindly requested. Blame taken too,
+ * but not liability.
+ */
+
+#define decshake(bits) \
+  int shake##bits(uint8_t*, size_t, const uint8_t*, size_t);
+
+#define decsha3(bits) \
+  int sha3_##bits(uint8_t*, size_t, const uint8_t*, size_t);
+
+decshake(128)
+decshake(256)
+decsha3(224)
+decsha3(256)
+decsha3(384)
+decsha3(512)
+
+}
 }
