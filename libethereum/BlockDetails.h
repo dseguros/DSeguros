480e3b5da8645db279b5c410a228b4d494086a1d
christopherdog <christopherdog@hotmail.com>
2021-07-08 05:50:01 +0800

    c_bloomIndexSize

--- libethereum/BlockDetails.h
+++ libethereum/BlockDetails.h
@@ -4,3 +4,19 @@
 #include <libdevcore/Log.h>
 #include <libdevcore/RLP.h>
 #include "TransactionReceipt.h"
+
+namespace dev
+{
+namespace eth
+{
+
+// TODO: OPTIMISE: constructors take bytes, RLP used only in necessary classes.
+
+static const unsigned c_bloomIndexSize = 16;
+static const unsigned c_bloomIndexLevels = 2;
+
+static const unsigned InvalidNumber = (unsigned)-1;
+
+}
+}
+
