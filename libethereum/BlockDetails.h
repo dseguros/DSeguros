b93c2ca30fb11fe5cc9e254bcc89a7a4ffa64946
christopherdog <christopherdog@hotmail.com>
2021-07-08 06:30:01 +0800

    BlockDetails

--- libethereum/BlockDetails.h
+++ libethereum/BlockDetails.h
@@ -17,6 +17,23 @@ static const unsigned c_bloomIndexLevels = 2;
 
 static const unsigned InvalidNumber = (unsigned)-1;
 
+struct BlockDetails
+{
+	BlockDetails(): number(InvalidNumber), totalDifficulty(Invalid256) {}
+	BlockDetails(unsigned _n, u256 _tD, h256 _p, h256s _c): number(_n), totalDifficulty(_tD), parent(_p), children(_c) {}
+	BlockDetails(RLP const& _r);
+	bytes rlp() const;
+
+	bool isNull() const { return number == InvalidNumber; }
+	explicit operator bool() const { return !isNull(); }
+
+	unsigned number = InvalidNumber;
+	u256 totalDifficulty = Invalid256;
+	h256 parent;
+	h256s children;
+
+	mutable unsigned size;
+};
 }
 }
 
