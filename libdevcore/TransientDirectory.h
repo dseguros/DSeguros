f7cf30e192050214d2afdeb9770112e858990733
samanthacarl999 <samanthacarl999@hotmail.com>
2021-07-09 07:50:01 +0800

    TransientDirectory.h added

---
+++ libdevcore/TransientDirectory.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+namespace dev
+{
+
+/**
+ * @brief temporary directory implementation
+ * It creates temporary directory in the given path. On dealloc it removes the directory
+ * @throws if the given path already exists, throws an exception
+ */
+class TransientDirectory
+{
+
+}
