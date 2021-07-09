a3046bd4303afc2f9423e6ca98ffb5a80ca1382f
samanthacarl999 <samanthacarl999@hotmail.com>
2021-07-09 09:00:01 +0800

    TransientDirectory

--- libdevcore/TransientDirectory.h
+++ libdevcore/TransientDirectory.h
@@ -12,5 +12,15 @@ namespace dev
  */
 class TransientDirectory
 {
+ public:
+	TransientDirectory();
+	TransientDirectory(std::string const& _path);
+	~TransientDirectory();
+
+	std::string const& path() const { return m_path; }
+
+private:
+	std::string m_path;
+};
 
 }
