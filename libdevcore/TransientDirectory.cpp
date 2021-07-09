32f6179e95ea1d9119b39eeefaca7c5b9a04c43f
samanthacarl999 <samanthacarl999@hotmail.com>
2021-07-09 10:10:01 +0800

    TransientDirectory::TransientDirectory

--- libdevcore/TransientDirectory.cpp
+++ libdevcore/TransientDirectory.cpp
@@ -8,3 +8,20 @@
 using namespace std;
 using namespace dev;
 namespace fs = boost::filesystem;
+
+TransientDirectory::TransientDirectory():
+	TransientDirectory((boost::filesystem::temp_directory_path() / "eth_transient" / toString(FixedHash<4>::random())).string())
+{}
+
+TransientDirectory::TransientDirectory(std::string const& _path):
+	m_path(_path)
+{
+	// we never ever want to delete a directory (including all its contents) that we did not create ourselves.
+	if (boost::filesystem::exists(m_path))
+		BOOST_THROW_EXCEPTION(FileError());
+
+	if (!fs::create_directories(m_path))
+		BOOST_THROW_EXCEPTION(FileError());
+	DEV_IGNORE_EXCEPTIONS(fs::permissions(m_path, fs::owner_all));
+}
+
