84c8b4c68eedc5b2f07010c013cba41df27f41c3
samanthacarl999 <samanthacarl999@hotmail.com>
2021-07-12 07:50:01 +0800

    sharedNibbles

--- libdevcore/TrieCommon.h
+++ libdevcore/TrieCommon.h
@@ -11,4 +11,18 @@ inline byte nibble(bytesConstRef _data, unsigned _i)
 	return (_i & 1) ? (_data[_i / 2] & 15) : (_data[_i / 2] >> 4);
 }
 
+/// Interprets @a _first and @a _second as vectors of nibbles and returns the length of the longest common
+/// prefix of _first[_beginFirst..._endFirst] and _second[_beginSecond..._endSecond].
+inline unsigned sharedNibbles(bytesConstRef _first, unsigned _beginFirst, unsigned _endFirst, bytesConstRef _second, unsigned _beginSecond, unsigned _endSecond)
+{
+	unsigned ret = 0;
+	while (_beginFirst < _endFirst && _beginSecond < _endSecond && nibble(_first, _beginFirst) == nibble(_second, _beginSecond))
+	{
+		++_beginFirst;
+		++_beginSecond;
+		++ret;
+	}
+	return ret;
+}
+
 }
