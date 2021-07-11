23346f2e298aa26673c870f0039ef0f74220caae
christopherdog <christopherdog@hotmail.com>
2021-07-12 05:00:01 +0800

    ChainParams.h

---
+++ libethereum/ChainParams.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <libdevcore/Common.h>
+#include <libethcore/Common.h>
+#include <libethcore/ChainOperationParams.h>
+#include <libethcore/BlockHeader.h>
+#include "Account.h"
+
+namespace dev
+{
+namespace eth
+{
+
+
+}
+}
