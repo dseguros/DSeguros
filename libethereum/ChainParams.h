064e1be0573aa5169049051fae7410fdf41242c5
christopherdog <christopherdog@hotmail.com>
2021-07-12 05:20:01 +0800

    ChainParams

--- libethereum/ChainParams.h
+++ libethereum/ChainParams.h
@@ -11,6 +11,29 @@ namespace dev
 namespace eth
 {
 
+class SealEngineFace;
 
+struct ChainParams: public ChainOperationParams
+{
+	ChainParams();
+	ChainParams(ChainParams const& /*_org*/) = default;
+	ChainParams(std::string const& _s, h256 const& _stateRoot = h256());
+	ChainParams(bytes const& _genesisRLP, AccountMap const& _state) { populateFromGenesis(_genesisRLP, _state); }
+	ChainParams(std::string const& _json, bytes const& _genesisRLP, AccountMap const& _state): ChainParams(_json) { populateFromGenesis(_genesisRLP, _state); }
+
+	SealEngineFace* createSealEngine();
+
+	/// Genesis params.
+	h256 parentHash = h256();
+	Address author = Address();
+	u256 difficulty = 1;
+	u256 gasLimit = 1 << 31;
+	u256 gasUsed = 0;
+	u256 timestamp = 0;
+	bytes extraData;
+	mutable h256 stateRoot;	///< Only pre-populate if known equivalent to genesisState's root. If they're different Bad Things Will Happen.
+	AccountMap genesisState;
+
+};
 }
 }
