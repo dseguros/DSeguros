5cdd62da1e5a36b954b6831f2042e6a8467236a8
samanthacarl999 <samanthacarl999@hotmail.com>
2021-07-12 09:00:01 +0800

    shared

--- libdevcore/TrieCommon.h
+++ libdevcore/TrieCommon.h
@@ -25,4 +25,44 @@ inline unsigned sharedNibbles(bytesConstRef _first, unsigned _beginFirst, unsign
 	return ret;
 }
 
+/**
+ * Nibble-based view on a bytesConstRef.
+ */
+struct NibbleSlice
+{
+	bytesConstRef data;
+	unsigned offset;
+
+	NibbleSlice(bytesConstRef _data = bytesConstRef(), unsigned _offset = 0): data(_data), offset(_offset) {}
+	byte operator[](unsigned _index) const { return nibble(data, offset + _index); }
+	unsigned size() const { return data.size() * 2 - offset; }
+	bool empty() const { return !size(); }
+	NibbleSlice mid(unsigned _index) const { return NibbleSlice(data, offset + _index); }
+	void clear() { data.reset(); offset = 0; }
+
+	/// @returns true iff _k is a prefix of this.
+	bool contains(NibbleSlice _k) const { return shared(_k) == _k.size(); }
+	/// @returns the number of shared nibbles at the beginning of this and _k.
+	unsigned shared(NibbleSlice _k) const { return sharedNibbles(data, offset, offset + size(), _k.data, _k.offset, _k.offset + _k.size()); }
+	/**
+	 * @brief Determine if we, a full key, are situated prior to a particular key-prefix.
+	 * @param _k The prefix.
+	 * @return true if we are strictly prior to the prefix.
+	 */
+	bool isEarlierThan(NibbleSlice _k) const
+	{
+		unsigned i = 0;
+		for (; i < _k.size() && i < size(); ++i)
+			if (operator[](i) < _k[i])		// Byte is lower - we're earlier..
+				return true;
+			else if (operator[](i) > _k[i])	// Byte is higher - we're not earlier.
+				return false;
+		if (i >= _k.size())					// Ran past the end of the prefix - we're == for the entire prefix - we're not earlier.
+			return false;
+		return true;						// Ran out before the prefix had finished - we're earlier.
+	}
+	bool operator==(NibbleSlice _k) const { return _k.size() == size() && shared(_k) == _k.size(); }
+	bool operator!=(NibbleSlice _s) const { return !operator==(_s); }
+};
+
 }
