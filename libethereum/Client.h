91cb5e4fe83e4621b92ddc1e047a0cddd97ed7f5
christopherdog <christopherdog@hotmail.com>
2021-07-15 05:20:01 +0800

    Client.h added

---
+++ libethereum/Client.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <thread>
+#include <condition_variable>
+#include <mutex>
+#include <list>
+#include <queue>
+#include <atomic>
+#include <string>
+#include <array>
+#include <libdevcore/Common.h>
+#include <libdevcore/CommonIO.h>
+#include <libdevcore/Guards.h>
+#include <libdevcore/Worker.h>
+#include <libethcore/SealEngine.h>
+#include <libethcore/ABI.h>
+#include <libp2p/Common.h>
+#include "BlockChain.h"
+#include "Block.h"
+#include "CommonNet.h"
+#include "ClientBase.h"
+
+namespace dev
+{
+namespace eth
+{
+
+
+}
+}
