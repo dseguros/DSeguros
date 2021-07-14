21f11f8d1f7268902f410353dc1c98576099df67
christopherdog <christopherdog@hotmail.com>
2021-07-15 06:30:01 +0800

    class Client

--- libethereum/Client.h
+++ libethereum/Client.h
@@ -40,5 +40,41 @@ struct ClientChat: public LogChannel { static const char* name(); static const i
 struct ClientTrace: public LogChannel { static const char* name(); static const int verbosity = 7; };
 struct ClientDetail: public LogChannel { static const char* name(); static const int verbosity = 14; };
 
+struct ActivityReport
+{
+	unsigned ticks = 0;
+	std::chrono::system_clock::time_point since = std::chrono::system_clock::now();
+};
+
+std::ostream& operator<<(std::ostream& _out, ActivityReport const& _r);
+
+/**
+ * @brief Main API hub for interfacing with Ethereum.
+ */
+class Client: public ClientBase, protected Worker
+{
+public:
+	Client(
+		ChainParams const& _params,
+		int _networkID,
+		p2p::Host* _host,
+		std::shared_ptr<GasPricer> _gpForAdoption,
+		std::string const& _dbPath = std::string(),
+		WithExisting _forceAction = WithExisting::Trust,
+		TransactionQueue::Limits const& _l = TransactionQueue::Limits{1024, 1024}
+	);
+	/// Destructor.
+	virtual ~Client();
+
+	/// Get information on this chain.
+	ChainParams const& chainParams() const { return bc().chainParams(); }
+
+	/// Resets the gas pricer to some other object.
+	void setGasPricer(std::shared_ptr<GasPricer> _gp) { m_gp = _gp; }
+	std::shared_ptr<GasPricer> gasPricer() const { return m_gp; }
+
+
+};
+
 }
 }
