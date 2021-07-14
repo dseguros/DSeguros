cd207891226c98697db2dc5f541b7b656be9925c
christopherdog <christopherdog@hotmail.com>
2021-07-15 05:50:01 +0800

    ClientWorkState

--- libethereum/Client.h
+++ libethereum/Client.h
@@ -25,6 +25,20 @@ namespace dev
 namespace eth
 {
 
+class Client;
+class DownloadMan;
+
+enum ClientWorkState
+{
+	Active = 0,
+	Deleting,
+	Deleted
+};
+
+struct ClientNote: public LogChannel { static const char* name(); static const int verbosity = 2; };
+struct ClientChat: public LogChannel { static const char* name(); static const int verbosity = 4; };
+struct ClientTrace: public LogChannel { static const char* name(); static const int verbosity = 7; };
+struct ClientDetail: public LogChannel { static const char* name(); static const int verbosity = 14; };
 
 }
 }
