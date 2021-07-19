83c9a3b9d63a17c8108a6d1efe13024cb0bb0417
christopherdog <christopherdog@hotmail.com>
2021-07-20 05:00:01 +0800

    Client

--- libethereum/Client.cpp
+++ libethereum/Client.cpp
@@ -23,3 +23,36 @@ std::ostream& dev::eth::operator<<(std::ostream& _out, ActivityReport const& _r)
 	_out << "): " << _r.ticks << "ticks";
 	return _out;
 }
+
+#if defined(_WIN32)
+const char* ClientNote::name() { return EthTeal "^" EthBlue " i"; }
+const char* ClientChat::name() { return EthTeal "^" EthWhite " o"; }
+const char* ClientTrace::name() { return EthTeal "^" EthGray " O"; }
+const char* ClientDetail::name() { return EthTeal "^" EthCoal " 0"; }
+#else
+const char* ClientNote::name() { return EthTeal "⧫" EthBlue " ℹ"; }
+const char* ClientChat::name() { return EthTeal "⧫" EthWhite " ◌"; }
+const char* ClientTrace::name() { return EthTeal "⧫" EthGray " ◎"; }
+const char* ClientDetail::name() { return EthTeal "⧫" EthCoal " ●"; }
+#endif
+
+Client::Client(
+	ChainParams const& _params,
+	int _networkID,
+	p2p::Host* _host,
+	std::shared_ptr<GasPricer> _gpForAdoption,
+	std::string const& _dbPath,
+	WithExisting _forceAction,
+	TransactionQueue::Limits const& _l
+):
+	ClientBase(_l),
+	Worker("eth", 0),
+	m_bc(_params, _dbPath, _forceAction, [](unsigned d, unsigned t){ std::cerr << "REVISING BLOCKCHAIN: Processed " << d << " of " << t << "...\r"; }),
+	m_gp(_gpForAdoption ? _gpForAdoption : make_shared<TrivialGasPricer>()),
+	m_preSeal(chainParams().accountStartNonce),
+	m_postSeal(chainParams().accountStartNonce),
+	m_working(chainParams().accountStartNonce)
+{
+	init(_host, _dbPath, _forceAction, _networkID);
+}
+
