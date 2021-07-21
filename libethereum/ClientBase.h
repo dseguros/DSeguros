6556669aa6ebea7d38a4b9e2438805e173eb9cb5
christopherdog <christopherdog@hotmail.com>
2021-07-22 05:50:01 +0800

    ClientBase.h added

---
+++ libethereum/ClientBase.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <chrono>
+#include "Interface.h"
+#include "LogFilter.h"
+#include "TransactionQueue.h"
+#include "Block.h"
+#include "CommonNet.h"
+
+namespace dev
+{
+
+namespace eth
+{
+
+
+}}
