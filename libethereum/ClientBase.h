b0cb61d23166c34c9a1f3289f73c0d1fa95ad243
christopherdog <christopherdog@hotmail.com>
2021-07-22 09:00:01 +0800

    Interface::create

--- libethereum/ClientBase.h
+++ libethereum/ClientBase.h
@@ -51,4 +51,38 @@ struct WorkChannel: public LogChannel { static const char* name(); static const 
 #define cworkin LogOutputStream<WorkInChannel, true>()
 #define cworkout LogOutputStream<WorkOutChannel, true>()
 
+class ClientBase: public Interface
+{
+public:
+	ClientBase(TransactionQueue::Limits const& _l = TransactionQueue::Limits{1024, 1024}): m_tq(_l) {}
+	virtual ~ClientBase() {}
+
+	/// Submits the given transaction.
+	/// @returns the new transaction's hash.
+	virtual std::pair<h256, Address> submitTransaction(TransactionSkeleton const& _t, Secret const& _secret) override;
+	using Interface::submitTransaction;
+
+	/// Makes the given call. Nothing is recorded into the state.
+	virtual ExecutionResult call(Address const& _secret, u256 _value, Address _dest, bytes const& _data, u256 _gas, u256 _gasPrice, BlockNumber _blockNumber, FudgeFactor _ff = FudgeFactor::Strict) override;
+	using Interface::call;
+
+	/// Makes the given create. Nothing is recorded into the state.
+	virtual ExecutionResult create(Address const& _secret, u256 _value, bytes const& _data, u256 _gas, u256 _gasPrice, BlockNumber _blockNumber, FudgeFactor _ff = FudgeFactor::Strict) override;
+
+	/// Estimate gas usage for call/create.
+	/// @param _maxGas An upper bound value for estimation, if not provided default value of c_maxGasEstimate will be used.
+	/// @param _callback Optional callback function for progress reporting
+	virtual std::pair<u256, ExecutionResult> estimateGas(Address const& _from, u256 _value, Address _dest, bytes const& _data, int64_t _maxGas, u256 _gasPrice, BlockNumber _blockNumber, GasEstimationCallback const& _callback) override;
+
+	using Interface::create;
+
+	using Interface::balanceAt;
+	using Interface::countAt;
+	using Interface::stateAt;
+	using Interface::codeAt;
+	using Interface::codeHashAt;
+	using Interface::storageAt;
+
+}
+
 }}
