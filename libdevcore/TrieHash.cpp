52f0de5dde2c9d04c5ec1e840428643af494f7f6
samanthacarl999 <samanthacarl999@hotmail.com>
2021-07-23 05:50:01 +0800

    TireHash

---
+++ libdevcore/TrieHash.cpp
@@ -0,0 +1,16 @@
+#if !defined(ETH_EMSCRIPTEN)
+
+#include "TrieHash.h"
+#include "TrieCommon.h"
+#include "TrieDB.h"	// @TODO replace ASAP!
+#include "SHA3.h"
+using namespace std;
+using namespace dev;
+
+namespace dev
+{
+
+
+}
+
+#endif // ETH_EMSCRIPTEN
