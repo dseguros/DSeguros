d241dc3730ff3489c0e72af2b77a32fb9e2b3401
samanthacarl999 <samanthacarl999@hotmail.com>
2021-07-23 08:45:29 +0800

    TrieDB.cpp added

---
+++ libdevcore/TrieDB.cpp
@@ -0,0 +1,13 @@
+#if !defined(ETH_EMSCRIPTEN)
+
+#include "Common.h"
+#include "TrieDB.h"
+using namespace std;
+using namespace dev;
+
+h256 const dev::c_shaNull = sha3(rlp(""));
+h256 const dev::EmptyTrie = sha3(rlp(""));
+
+const char* TrieDBChannel::name() { return "-T-"; }
+
+#endif // ETH_EMSCRIPTEN
