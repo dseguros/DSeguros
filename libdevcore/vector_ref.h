5c8a317069a6f88bc4ac199beafab56bd0cdbe6b
samanthacarl999 <samanthacarl999@hotmail.com>
2021-07-27 07:50:01 +0800

    vector_ref.h

---
+++ libdevcore/vector_ref.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <cstring>
+#include <cassert>
+#include <type_traits>
+#include <vector>
+#include <string>
+
+#ifdef __INTEL_COMPILER
+#pragma warning(disable:597) //will not be called for implicit or explicit conversions
+#endif
+
+namespace dev
+{
+
+}
