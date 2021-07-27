17c65c697223871ea6b17638d2f6e97c99c2ba52
christopherdog <christopherdog@hotmail.com>
2021-07-28 05:50:01 +0800

    ClientTest.h

---
+++ libethereum/ClientTest.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#include <tuple>
+#include <libethereum/Client.h>
+
+namespace dev
+{
+namespace eth
+{
+
+DEV_SIMPLE_EXCEPTION(ChainParamsInvalid);
+DEV_SIMPLE_EXCEPTION(ChainParamsNotNoProof);
+
+
+}
