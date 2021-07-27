3233be3ca8bd20dc17a34401374b4272f721f34b
christopherdog <christopherdog@hotmail.com>
2021-07-28 06:30:01 +0800

    ClientTest

--- libethereum/ClientTest.h
+++ libethereum/ClientTest.h
@@ -12,5 +12,26 @@ namespace eth
 DEV_SIMPLE_EXCEPTION(ChainParamsInvalid);
 DEV_SIMPLE_EXCEPTION(ChainParamsNotNoProof);
 
+class ClientTest: public Client
+{
+public:
+	/// Trivial forwarding constructor.
+	ClientTest(
+		ChainParams const& _params,
+		int _networkID,
+		p2p::Host* _host,
+		std::shared_ptr<GasPricer> _gpForAdoption,
+		std::string const& _dbPath = std::string(),
+		WithExisting _forceAction = WithExisting::Trust,
+		TransactionQueue::Limits const& _l = TransactionQueue::Limits{1024, 1024}
+	);
+
+	void setChainParams(std::string const& _genesis);
+	void mineBlocks(unsigned _count);
+	void modifyTimestamp(u256 const& _timestamp);
+	void rewindToBlock(unsigned _number);
+	bool addBlock(std::string const& _rlp);
+	bool completeSync();
+};
 
 }
