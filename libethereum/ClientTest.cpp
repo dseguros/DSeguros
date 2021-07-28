e9a747b8e53ab0fe87e3e8ee3c0feb446e3bbf11
christopherdog <christopherdog@hotmail.com>
2021-07-28 09:00:01 +0800

    asClientTest

--- libethereum/ClientTest.cpp
+++ libethereum/ClientTest.cpp
@@ -6,3 +6,12 @@ using namespace dev;
 using namespace dev::eth;
 using namespace p2p;
 
+ClientTest& dev::eth::asClientTest(Interface& _c)
+{
+	return dynamic_cast<ClientTest&>(_c);
+}
+
+ClientTest* dev::eth::asClientTest(Interface* _c)
+{
+	return &dynamic_cast<ClientTest&>(*_c);
+}
