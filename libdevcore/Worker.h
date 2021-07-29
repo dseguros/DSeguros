9ca12cb4ba7d3e6ba4374a7053270ba9e6ba274c
samanthacarl999 <samanthacarl999@hotmail.com>
2021-07-29 09:20:01 +0800

    operator

--- libdevcore/Worker.h
+++ libdevcore/Worker.h
@@ -25,4 +25,21 @@ enum class WorkerState
 	Killing
 };
 
+class Worker
+{
+protected:
+	Worker(std::string const& _name = "anon", unsigned _idleWaitMs = 30): m_name(_name), m_idleWaitMs(_idleWaitMs) {}
+
+	/// Move-constructor.
+	Worker(Worker&& _m) { std::swap(m_name, _m.m_name); }
+
+	/// Move-assignment.
+	Worker& operator=(Worker&& _m)
+	{
+		assert(&_m != this);
+		std::swap(m_name, _m.m_name);
+		return *this;
+	}
+
+};
 }
