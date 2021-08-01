e8125339ebdadab429c963c04fbb8216a5408e81
christopherdog <christopherdog@hotmail.com>
2021-08-02 07:50:01 +0800

    SubprotocolPacketType

--- libethereum/CommonNet.h
+++ libethereum/CommonNet.h
@@ -34,5 +34,24 @@ class TransactionQueue;
 class EthereumHost;
 class EthereumPeer;
 
+enum SubprotocolPacketType: byte
+{
+	StatusPacket = 0x00,
+	NewBlockHashesPacket = 0x01,
+	TransactionsPacket = 0x02,
+	GetBlockHeadersPacket = 0x03,
+	BlockHeadersPacket = 0x04,
+	GetBlockBodiesPacket = 0x05,
+	BlockBodiesPacket = 0x06,
+	NewBlockPacket = 0x07,
+
+	GetNodeDataPacket = 0x0d,
+	NodeDataPacket = 0x0e,
+	GetReceiptsPacket = 0x0f,
+	ReceiptsPacket = 0x10,
+
+	PacketCount
+};
+
 }
 }
