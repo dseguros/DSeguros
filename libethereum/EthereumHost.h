666efcafb1cc8d02125653923eafb59efa99fbb3
christopherdog <christopherdog@hotmail.com>
2021-08-04 06:30:01 +0800

    EthereumHost

---
+++ libethereum/EthereumHost.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <mutex>
+#include <unordered_map>
+#include <vector>
+#include <unordered_set>
+#include <memory>
+#include <utility>
+#include <thread>
+
+#include <libdevcore/Guards.h>
+#include <libdevcore/Worker.h>
+#include <libethcore/Common.h>
+#include <libp2p/Common.h>
+#include <libdevcore/OverlayDB.h>
+#include <libethcore/BlockHeader.h>
+#include <libethereum/BlockChainSync.h>
+#include "CommonNet.h"
+#include "EthereumPeer.h"
+
+namespace dev
+{
+
+}
