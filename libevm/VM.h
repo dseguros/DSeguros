7bf2082ea5f0e910ffc0ea567f54e4a450dde09a
samanthacarl999 <samanthacarl999@hotmail.com>
2021-08-05 06:30:01 +0800

    VM.h

---
+++ libevm/VM.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <unordered_map>
+#include <libdevcore/Exceptions.h>
+#include <libethcore/Common.h>
+#include <libevmcore/Instruction.h>
+#include <libdevcore/SHA3.h>
+#include <libethcore/BlockHeader.h>
+#include "VMFace.h"
+
+namespace dev
+{
+
+}
