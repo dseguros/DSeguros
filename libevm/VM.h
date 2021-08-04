658083a779d390d9b12ccbf20dfd909a9dfa1c1c
samanthacarl999 <samanthacarl999@hotmail.com>
2021-08-05 07:20:01 +0800

    asAddress

--- libevm/VM.h
+++ libevm/VM.h
@@ -11,4 +11,15 @@
 namespace dev
 {
 
+namespace eth
+{
+
+// Convert from a 256-bit integer stack/memory entry into a 160-bit Address hash.
+// Currently we just pull out the right (low-order in BE) 160-bits.
+inline Address asAddress(u256 _item)
+{
+	return right160(h256(_item));
+}
+
+}
 }
