ea0b3945f314947b24f2384b6087d8ae09c8d464
samanthacarl999 <samanthacarl999@hotmail.com>
2021-08-05 09:20:01 +0800

    class VM

--- libevm/VM.h
+++ libevm/VM.h
@@ -33,5 +33,25 @@ struct InstructionMetric
 	int ret;
 };
 
+class VM: public VMFace
+{
+public:
+	virtual owning_bytes_ref exec(u256& _io_gas, ExtVMFace& _ext, OnOpFunc const& _onOp) override final;
+
+#if EVM_JUMPS_AND_SUBS
+	// invalid code will throw an exeption
+	void validate(ExtVMFace& _ext);
+	void validateSubroutine(uint64_t _PC, uint64_t* _rp, u256* _sp);
+#endif
+
+	bytes const& memory() const { return m_mem; }
+	u256s stack() const {
+		u256s stack(m_SP, m_stackEnd);
+		reverse(stack.begin(), stack.end());
+		return stack;
+	};
+
+};
+
 }
 }
