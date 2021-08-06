aa59e357e707434ab2bfdf38333b1896da82df29
samanthacarl999 <samanthacarl999@hotmail.com>
2021-08-06 09:00:01 +0800

    decodeJumpvDest

--- libevm/VM.cpp
+++ libevm/VM.cpp
@@ -6,4 +6,51 @@ using namespace std;
 using namespace dev;
 using namespace dev::eth;
 
+uint64_t VM::memNeed(u256 _offset, u256 _size)
+{
+	return toInt63(_size ? u512(_offset) + _size : u512(0));
+}
+
+template <class S> S divWorkaround(S const& _a, S const& _b)
+{
+	return (S)(s512(_a) / s512(_b));
+}
+
+template <class S> S modWorkaround(S const& _a, S const& _b)
+{
+	return (S)(s512(_a) % s512(_b));
+}
+
+
+//
+// for decoding destinations of JUMPTO, JUMPV, JUMPSUB and JUMPSUBV
+//
+
+uint64_t VM::decodeJumpDest(const byte* const _code, uint64_t& _pc)
+{
+	// turn 4 MSB-first bytes in the code into a native-order integer
+	uint64_t dest      = _code[_pc++];
+	dest = (dest << 8) | _code[_pc++];
+	dest = (dest << 8) | _code[_pc++];
+	dest = (dest << 8) | _code[_pc++];
+	return dest;
+}
+
+uint64_t VM::decodeJumpvDest(const byte* const _code, uint64_t& _pc, byte _voff)
+{
+	// Layout of jump table in bytecode...
+	//     byte opcode
+	//     byte n_jumps
+	//     byte table[n_jumps][4]
+	//	
+	uint64_t pc = _pc;
+	byte n = _code[++pc];           // byte after opcode is number of jumps
+	if (_voff >= n) _voff = n - 1;  // if offset overflows use default jump
+	pc += _voff * 4;                // adjust inout pc before index destination in table
+	
+	uint64_t dest = decodeJumpDest(_code, pc);
+	
+	_pc += 1 + n * 4;               // adust inout _pc to opcode after table 
+	return dest;
+}
 
