ef03a215976f7c14efe67531658dc53e228e214e
christopherdog <christopherdog@hotmail.com>
2021-08-10 07:20:01 +0800

    class and method onPeerStatus

--- libethereum/EthereumPeer.h
+++ libethereum/EthereumPeer.h
@@ -16,6 +16,29 @@ namespace dev
 namespace eth
 {
 
+class EthereumPeerObserverFace
+{
+public:
+	virtual ~EthereumPeerObserverFace() {}
+
+	virtual void onPeerStatus(std::shared_ptr<EthereumPeer> _peer) = 0;
+
+	virtual void onPeerTransactions(std::shared_ptr<EthereumPeer> _peer, RLP const& _r) = 0;
+
+	virtual void onPeerBlockHeaders(std::shared_ptr<EthereumPeer> _peer, RLP const& _headers) = 0;
+
+	virtual void onPeerBlockBodies(std::shared_ptr<EthereumPeer> _peer, RLP const& _r) = 0;
+
+	virtual void onPeerNewHashes(std::shared_ptr<EthereumPeer> _peer, std::vector<std::pair<h256, u256>> const& _hashes) = 0;
+
+	virtual void onPeerNewBlock(std::shared_ptr<EthereumPeer> _peer, RLP const& _r) = 0;
+
+	virtual void onPeerNodeData(std::shared_ptr<EthereumPeer> _peer, RLP const& _r) = 0;
+
+	virtual void onPeerReceipts(std::shared_ptr<EthereumPeer> _peer, RLP const& _r) = 0;
+
+	virtual void onPeerAborting() = 0;
+};
 
 }
 }
