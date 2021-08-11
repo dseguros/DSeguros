5726e42fb63cb320590c3e57007394697928494b
samanthacarl999 <samanthacarl999@hotmail.com>
2021-08-11 09:00:01 +0800

    VMException

--- libevm/VMFace.h
+++ libevm/VMFace.h
@@ -9,5 +9,13 @@ namespace dev
 namespace eth
 {
 
+struct VMException: Exception {};
+#define ETH_SIMPLE_EXCEPTION_VM(X) struct X: VMException { const char* what() const noexcept override { return #X; } }
+ETH_SIMPLE_EXCEPTION_VM(BadInstruction);
+ETH_SIMPLE_EXCEPTION_VM(BadJumpDestination);
+ETH_SIMPLE_EXCEPTION_VM(OutOfGas);
+ETH_SIMPLE_EXCEPTION_VM(OutOfStack);
+ETH_SIMPLE_EXCEPTION_VM(StackUnderflow);
+
 }
 }
