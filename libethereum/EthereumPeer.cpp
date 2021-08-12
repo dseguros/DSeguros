7ace7caf0461f36f6c90fbe423ca7209cd350206
christopherdog <christopherdog@hotmail.com>
2021-08-12 11:40:01 +0800

    interpret

--- libethereum/EthereumPeer.cpp
+++ libethereum/EthereumPeer.cpp
@@ -135,3 +135,234 @@ void EthereumPeer::requestByHashes(h256s const& _hashes, Asking _asking, Subprot
 	else
 		setIdle();
 }
+
+void EthereumPeer::setAsking(Asking _a)
+{
+	m_asking = _a;
+	m_lastAsk = std::chrono::system_clock::to_time_t(chrono::system_clock::now());
+
+	auto s = session();
+	if (s)
+	{
+		s->addNote("ask", ::toString(_a));
+		s->addNote("sync", string(isCriticalSyncing() ? "ONGOING" : "holding") + (needsSyncing() ? " & needed" : ""));
+	}
+}
+
+void EthereumPeer::tick()
+{
+	auto s = session();
+	time_t  now = std::chrono::system_clock::to_time_t(chrono::system_clock::now());
+	if (s && (now - m_lastAsk > 10 && m_asking != Asking::Nothing))
+		// timeout
+		s->disconnect(PingTimeout);
+}
+
+bool EthereumPeer::isConversing() const
+{
+	return m_asking != Asking::Nothing;
+}
+
+bool EthereumPeer::isCriticalSyncing() const
+{
+	return m_asking == Asking::BlockHeaders || m_asking == Asking::State || (m_asking == Asking::BlockBodies && m_protocolVersion == 62);
+}
+
+bool EthereumPeer::interpret(unsigned _id, RLP const& _r)
+{
+	assert(m_observer);
+
+	m_lastAsk = std::chrono::system_clock::to_time_t(chrono::system_clock::now());
+	try
+	{
+	switch (_id)
+	{
+	case StatusPacket:
+	{
+		m_protocolVersion = _r[0].toInt<unsigned>();
+		m_networkId = _r[1].toInt<u256>();
+		m_totalDifficulty = _r[2].toInt<u256>();
+		m_latestHash = _r[3].toHash<h256>();
+		m_genesisHash = _r[4].toHash<h256>();
+		if (m_peerCapabilityVersion == m_hostProtocolVersion)
+			m_protocolVersion = m_hostProtocolVersion;
+
+		clog(NetMessageSummary) << "Status:" << m_protocolVersion << "/" << m_networkId << "/" << m_genesisHash << ", TD:" << m_totalDifficulty << "=" << m_latestHash;
+		setIdle();
+		m_observer->onPeerStatus(dynamic_pointer_cast<EthereumPeer>(dynamic_pointer_cast<EthereumPeer>(shared_from_this())));
+		break;
+	}
+	case TransactionsPacket:
+	{
+		m_observer->onPeerTransactions(dynamic_pointer_cast<EthereumPeer>(dynamic_pointer_cast<EthereumPeer>(shared_from_this())), _r);
+		break;
+	}
+	case GetBlockHeadersPacket:
+	{
+		/// Packet layout:
+		/// [ block: { P , B_32 }, maxHeaders: P, skip: P, reverse: P in { 0 , 1 } ]
+		const auto blockId = _r[0];
+		const auto maxHeaders = _r[1].toInt<u256>();
+		const auto skip = _r[2].toInt<u256>();
+		const auto reverse = _r[3].toInt<bool>();
+
+		auto numHeadersToSend = maxHeaders <= c_maxHeadersToSend ? static_cast<unsigned>(maxHeaders) : c_maxHeadersToSend;
+
+		if (skip > std::numeric_limits<unsigned>::max() - 1)
+		{
+			clog(NetAllDetail) << "Requested block skip is too big: " << skip;
+			break;
+		}
+
+		pair<bytes, unsigned> const rlpAndItemCount = m_hostData->blockHeaders(blockId, numHeadersToSend, skip, reverse);
+
+		RLPStream s;
+		prep(s, BlockHeadersPacket, rlpAndItemCount.second).appendRaw(rlpAndItemCount.first, rlpAndItemCount.second);
+		sealAndSend(s);
+		addRating(0);
+		break;
+	}
+	case BlockHeadersPacket:
+	{
+		if (m_asking != Asking::BlockHeaders)
+			clog(NetImpolite) << "Peer giving us block headers when we didn't ask for them.";
+		else
+		{
+			setIdle();
+			m_observer->onPeerBlockHeaders(dynamic_pointer_cast<EthereumPeer>(shared_from_this()), _r);
+		}
+		break;
+	}
+	case GetBlockBodiesPacket:
+	{
+		unsigned count = static_cast<unsigned>(_r.itemCount());
+		clog(NetMessageSummary) << "GetBlockBodies (" << dec << count << "entries)";
+
+		if (!count)
+		{
+			clog(NetImpolite) << "Zero-entry GetBlockBodies: Not replying.";
+			addRating(-10);
+			break;
+		}
+
+		pair<bytes, unsigned> const rlpAndItemCount = m_hostData->blockBodies(_r);
+
+		addRating(0);
+		RLPStream s;
+		prep(s, BlockBodiesPacket, rlpAndItemCount.second).appendRaw(rlpAndItemCount.first, rlpAndItemCount.second);
+		sealAndSend(s);
+		break;
+	}
+	case BlockBodiesPacket:
+	{
+		if (m_asking != Asking::BlockBodies)
+			clog(NetImpolite) << "Peer giving us block bodies when we didn't ask for them.";
+		else
+		{
+			setIdle();
+			m_observer->onPeerBlockBodies(dynamic_pointer_cast<EthereumPeer>(shared_from_this()), _r);
+		}
+		break;
+	}
+	case NewBlockPacket:
+	{
+		m_observer->onPeerNewBlock(dynamic_pointer_cast<EthereumPeer>(shared_from_this()), _r);
+		break;
+	}
+	case NewBlockHashesPacket:
+	{
+		unsigned itemCount = _r.itemCount();
+
+		clog(NetMessageSummary) << "BlockHashes (" << dec << itemCount << "entries)" << (itemCount ? "" : ": NoMoreHashes");
+
+		if (itemCount > c_maxIncomingNewHashes)
+		{
+			disable("Too many new hashes");
+			break;
+		}
+
+		vector<pair<h256, u256>> hashes(itemCount);
+		for (unsigned i = 0; i < itemCount; ++i)
+			hashes[i] = std::make_pair(_r[i][0].toHash<h256>(), _r[i][1].toInt<u256>());
+
+		m_observer->onPeerNewHashes(dynamic_pointer_cast<EthereumPeer>(shared_from_this()), hashes);
+		break;
+	}
+	case GetNodeDataPacket:
+	{
+		unsigned count = static_cast<unsigned>(_r.itemCount());
+		if (!count)
+		{
+			clog(NetImpolite) << "Zero-entry GetNodeData: Not replying.";
+			addRating(-10);
+			break;
+		}
+		clog(NetMessageSummary) << "GetNodeData (" << dec << count << " entries)";
+
+		strings const data = m_hostData->nodeData(_r);
+
+		addRating(0);
+		RLPStream s;
+		prep(s, NodeDataPacket, data.size());
+		for (auto const& element: data)
+			s.append(element);
+		sealAndSend(s);
+		break;
+	}
+	case GetReceiptsPacket:
+	{
+		unsigned count = static_cast<unsigned>(_r.itemCount());
+		if (!count)
+		{
+			clog(NetImpolite) << "Zero-entry GetReceipts: Not replying.";
+			addRating(-10);
+			break;
+		}
+		clog(NetMessageSummary) << "GetReceipts (" << dec << count << " entries)";
+
+		pair<bytes, unsigned> const rlpAndItemCount = m_hostData->receipts(_r);
+
+		addRating(0);
+		RLPStream s;
+		prep(s, ReceiptsPacket, rlpAndItemCount.second).appendRaw(rlpAndItemCount.first, rlpAndItemCount.second);
+		sealAndSend(s);
+		break;
+	}
+	case NodeDataPacket:
+	{
+		if (m_asking != Asking::NodeData)
+			clog(NetImpolite) << "Peer giving us node data when we didn't ask for them.";
+		else
+		{
+			setIdle();
+			m_observer->onPeerNodeData(dynamic_pointer_cast<EthereumPeer>(shared_from_this()), _r);
+		}
+		break;
+	}
+	case ReceiptsPacket:
+	{
+		if (m_asking != Asking::Receipts)
+			clog(NetImpolite) << "Peer giving us receipts when we didn't ask for them.";
+		else
+		{
+			setIdle();
+			m_observer->onPeerReceipts(dynamic_pointer_cast<EthereumPeer>(shared_from_this()), _r);
+		}
+		break;
+	}
+	default:
+		return false;
+	}
+	}
+	catch (Exception const&)
+	{
+		clog(NetWarn) << "Peer causing an Exception:" << boost::current_exception_diagnostic_information() << _r;
+	}
+	catch (std::exception const& _e)
+	{
+		clog(NetWarn) << "Peer causing an exception:" << _e.what() << _r;
+	}
+
+	return true;
+}
+
