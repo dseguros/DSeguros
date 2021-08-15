ef8b5bd4b9d20e40b29fcbe125120d50051dd272
christopherdog <christopherdog@hotmail.com>
2021-08-16 06:30:01 +0800

    Executive.h

---
+++ libethereum/Executive.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <functional>
+#include <json/json.h>
+#include <libdevcore/Log.h>
+#include <libevmcore/Instruction.h>
+#include <libethcore/Common.h>
+#include <libevm/VMFace.h>
+#include "Transaction.h"
+
+namespace Json
+{
+	class Value;
+}
+
