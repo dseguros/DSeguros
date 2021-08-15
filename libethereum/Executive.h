5d22aa753691af877dbe13b099d854de4c46cbf0
christopherdog <christopherdog@hotmail.com>
2021-08-16 07:20:01 +0800

    ExecutiveWarnChannel

--- libethereum/Executive.h
+++ libethereum/Executive.h
@@ -13,3 +13,25 @@ namespace Json
 	class Value;
 }
 
+
+namespace dev
+{
+
+class OverlayDB;
+
+namespace eth
+{
+
+class State;
+class Block;
+class BlockChain;
+class ExtVM;
+class SealEngineFace;
+struct Manifest;
+
+struct VMTraceChannel: public LogChannel { static const char* name(); static const int verbosity = 11; };
+struct ExecutiveWarnChannel: public LogChannel { static const char* name(); static const int verbosity = 1; };
+
+}
+}
+
