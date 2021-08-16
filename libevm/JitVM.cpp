03bdc806ac01c0fed9090b5d101eecb5e9d82aa9
samanthacarl999 <samanthacarl999@hotmail.com>
2021-08-17 07:20:01 +0800

    switch case

--- libevm/JitVM.cpp
+++ libevm/JitVM.cpp
@@ -44,6 +44,86 @@ inline h256 asHash(evm_uint256be _n)
 	return h256(&_n.bytes[0], h256::ConstructFromPointer);
 }
 
+void evm_query(
+	evm_variant* o_result,
+	evm_env* _opaqueEnv,
+	evm_query_key _key,
+	evm_variant const* _arg
+) noexcept
+{
+	auto &env = *reinterpret_cast<ExtVMFace*>(_opaqueEnv);
+	switch (_key)
+	{
+	case EVM_ADDRESS:
+		o_result->address = toEvmC(env.myAddress);
+		break;
+	case EVM_CALLER:
+		o_result->address = toEvmC(env.caller);
+		break;
+	case EVM_ORIGIN:
+		o_result->address = toEvmC(env.origin);
+		break;
+	case EVM_GAS_PRICE:
+		o_result->uint256be = toEvmC(env.gasPrice);
+		break;
+	case EVM_COINBASE:
+		o_result->address = toEvmC(env.envInfo().author());
+		break;
+	case EVM_DIFFICULTY:
+		o_result->uint256be = toEvmC(env.envInfo().difficulty());
+		break;
+	case EVM_GAS_LIMIT:
+		o_result->int64 = env.envInfo().gasLimit();
+		break;
+	case EVM_NUMBER:
+		// TODO: Handle overflow / exception
+		o_result->int64 = static_cast<int64_t>(env.envInfo().number());
+		break;
+	case EVM_TIMESTAMP:
+		// TODO: Handle overflow / exception
+		o_result->int64 = static_cast<int64_t>(env.envInfo().timestamp());
+		break;
+	case EVM_CODE_BY_ADDRESS:
+	{
+		auto addr = fromEvmC(_arg->address);
+		auto &code = env.codeAt(addr);
+		o_result->data = code.data();
+		o_result->data_size = code.size();
+		break;
+	}
+	case EVM_CODE_SIZE:
+	{
+		auto addr = fromEvmC(_arg->address);
+		o_result->int64 = env.codeSizeAt(addr);
+		break;
+	}
+	case EVM_BALANCE:
+	{
+		auto addr = fromEvmC(_arg->address);
+		o_result->uint256be = toEvmC(env.balance(addr));
+		break;
+	}
+	case EVM_BLOCKHASH:
+		o_result->uint256be = toEvmC(env.blockHash(_arg->int64));
+		break;
+	case EVM_SLOAD:
+	{
+		auto key = asUint(_arg->uint256be);
+		o_result->uint256be = toEvmC(env.store(key));
+		break;
+	}
+	case EVM_ACCOUNT_EXISTS:
+	{
+		auto addr = fromEvmC(_arg->address);
+		o_result->int64 = env.exists(addr);
+		break;
+	}
+	case EVM_CALL_DEPTH:
+		o_result->int64 = env.depth;
+		break;
+	}
+}
+
 }
 
 }
