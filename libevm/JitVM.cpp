280dc825742f27bdb52a1094d87f0a46d87e5571
samanthacarl999 <samanthacarl999@hotmail.com>
2021-08-17 05:20:01 +0800

    toEvmC

--- libevm/JitVM.cpp
+++ libevm/JitVM.cpp
@@ -11,6 +11,18 @@ namespace eth
 namespace
 {
 
+static_assert(sizeof(Address) == sizeof(evm_uint160be),
+              "Address types size mismatch");
+static_assert(alignof(Address) == alignof(evm_uint160be),
+              "Address types alignment mismatch");
+
+inline evm_uint160be toEvmC(Address _addr)
+{
+	return *reinterpret_cast<evm_uint160be*>(&_addr);
+}
+
+
+}
 
 }
 }
