cb03a820ceb10b2840c4f1c98f5427ab62d39ae3
christopherdog <christopherdog@hotmail.com>
2021-08-18 05:50:01 +0800

    Execute.cpp

---
+++ libethereum/Executive.cpp
@@ -0,0 +1,25 @@
+
+#include "Executive.h"
+
+#include <boost/timer.hpp>
+#include <json/json.h>
+#include <libdevcore/CommonIO.h>
+#include <libevm/VMFactory.h>
+#include <libevm/VM.h>
+#include <libethcore/CommonJS.h>
+#include "Interface.h"
+#include "State.h"
+#include "ExtVM.h"
+#include "BlockChain.h"
+#include "Block.h"
+using namespace std;
+using namespace dev;
+using namespace dev::eth;
+
+const char* VMTraceChannel::name() { return "EVM"; }
+const char* ExecutiveWarnChannel::name() { return WarnChannel::name(); }
+
+StandardTrace::StandardTrace():
+	m_trace(Json::arrayValue)
+{}
+
