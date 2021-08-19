4f870540176a7b23e179c141e179e89ddaf80c66
christopherdog <christopherdog@hotmail.com>
2021-08-20 07:50:01 +0800

    ExtVM.h

---
+++ libethereum/ExtVM.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <map>
+#include <functional>
+#include <libethcore/Common.h>
+#include <libevm/ExtVMFace.h>
+#include <libethcore/SealEngine.h>
+#include "State.h"
+#include "Executive.h"
+
+namespace dev
+{
+namespace eth
+{
+
+
+}
+}
+
