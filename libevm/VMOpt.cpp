c635599c2b21801fdf19775537fca99b99ba0292
samanthacarl999 <samanthacarl999@hotmail.com>
2021-08-23 07:50:01 +0800

    reportStackUse

--- libevm/VMOpt.cpp
+++ libevm/VMOpt.cpp
@@ -5,3 +5,11 @@ using namespace std;
 using namespace dev;
 using namespace dev::eth;
 
+void VM::reportStackUse()
+{
+	static intptr_t p = 0;
+	intptr_t q = intptr_t(&q);
+	if (p)
+		cerr << "STACK: " << p << " - " << q << " = " << (p - q) << endl;
+	p = q;
+}
