52b633520bc71eca8707251f288b7b81d9bb9d24
samanthacarl999 <samanthacarl999@hotmail.com>
2021-08-23 09:00:01 +0800

    initMetrics

--- libevm/VMOpt.cpp
+++ libevm/VMOpt.cpp
@@ -13,3 +13,20 @@ void VM::reportStackUse()
 		cerr << "STACK: " << p << " - " << q << " = " << (p - q) << endl;
 	p = q;
 }
+
+std::array<InstructionMetric, 256> VM::c_metrics;
+void VM::initMetrics()
+{
+	static bool done=false;
+	if (!done)
+	{
+		for (unsigned i = 0; i < 256; ++i)
+		{
+			InstructionInfo op = instructionInfo((Instruction)i);
+			c_metrics[i].gasPriceTier = op.gasPriceTier;
+			c_metrics[i].args = op.args;
+			c_metrics[i].ret = op.ret;
+		}
+	}
+	done = true;
+}
