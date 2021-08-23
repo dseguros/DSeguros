1dc18670f4e2c966848a63579cde251106c99ede
samanthacarl999 <samanthacarl999@hotmail.com>
2021-08-23 09:20:01 +0800

    copyCode

--- libevm/VMOpt.cpp
+++ libevm/VMOpt.cpp
@@ -30,3 +30,15 @@ void VM::initMetrics()
 	}
 	done = true;
 }
+
+void VM::copyCode(int _extraBytes)
+{
+	// Copy code so that it can be safely modified and extend code by
+	// _extraBytes zero bytes to allow reading virtual data at the end
+	// of the code without bounds checks.
+	auto extendedSize = m_ext->code.size() + _extraBytes;
+	m_code.reserve(extendedSize);
+	m_code = m_ext->code;
+	m_code.resize(extendedSize);
+}
+
