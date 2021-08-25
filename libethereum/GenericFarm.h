5692b63c1bed04f42d3fdec07c0cba974d690179
christopherdog <christopherdog@hotmail.com>
2021-08-26 05:50:01 +0800

    GenericFarmFace

--- libethereum/GenericFarm.h
+++ libethereum/GenericFarm.h
@@ -15,5 +15,25 @@ namespace dev
 namespace eth
 {
 
+template <class PoW>
+class GenericFarm: public GenericFarmFace<PoW>
+{
+public:
+	using WorkPackage = typename PoW::WorkPackage;
+	using Solution = typename PoW::Solution;
+	using Miner = GenericMiner<PoW>;
+
+	struct SealerDescriptor
+	{
+		std::function<unsigned()> instances;
+		std::function<Miner*(typename Miner::ConstructionInfo ci)> create;
+	};
+
+	~GenericFarm()
+	{
+		stop();
+	}
+};
+
 }
 }
