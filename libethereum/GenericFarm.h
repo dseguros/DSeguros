f67ac93cdf5aafdb7a80484b6888dafd5f06b22a
christopherdog <christopherdog@hotmail.com>
2021-08-26 05:20:01 +0800

    GenericFarm.h

---
+++ libethereum/GenericFarm.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <thread>
+#include <list>
+#include <atomic>
+#include <libdevcore/Common.h>
+#include <libdevcore/Worker.h>
+#include <libethcore/Common.h>
+#include <libethereum/GenericMiner.h>
+#include <libethcore/BlockHeader.h>
+
+namespace dev
+{
+
+namespace eth
+{
+
+}
+}
