cbff8dcfe18064830eeadda9d6628000c5adae3a
christopherdog <christopherdog@hotmail.com>
2021-08-26 06:30:01 +0800

    setSealers

--- libethereum/GenericFarm.h
+++ libethereum/GenericFarm.h
@@ -33,6 +33,23 @@ public:
 	{
 		stop();
 	}
+
+	/**
+	 * @brief Sets the current mining mission.
+	 * @param _wp The work package we wish to be mining.
+	 */
+	void setWork(WorkPackage const& _wp)
+	{
+		WriteGuard l(x_minerWork);
+		if (_wp.headerHash == m_work.headerHash)
+			return;
+		m_work = _wp;
+		for (auto const& m: m_miners)
+			m->setWork(m_work);
+		resetTimer();
+	}
+
+	void setSealers(std::map<std::string, SealerDescriptor> const& _sealers) { m_sealers = _sealers; }
 };
 
 }
