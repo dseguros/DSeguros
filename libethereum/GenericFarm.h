7f73ffdf3c48bdcee7d669ad627bbfedecc03d08
christopherdog <christopherdog@hotmail.com>
2021-08-26 07:20:01 +0800

    bool start

--- libethereum/GenericFarm.h
+++ libethereum/GenericFarm.h
@@ -50,6 +50,31 @@ public:
 	}
 
 	void setSealers(std::map<std::string, SealerDescriptor> const& _sealers) { m_sealers = _sealers; }
+
+	/**
+	 * @brief Start a number of miners.
+	 */
+	bool start(std::string const& _sealer)
+	{
+		WriteGuard l(x_minerWork);
+		if (!m_miners.empty() && m_lastSealer == _sealer)
+			return true;
+		if (!m_sealers.count(_sealer))
+			return false;
+
+		m_miners.clear();
+		auto ins = m_sealers[_sealer].instances();
+		m_miners.reserve(ins);
+		for (unsigned i = 0; i < ins; ++i)
+		{
+			m_miners.push_back(std::shared_ptr<Miner>(m_sealers[_sealer].create(std::make_pair(this, i))));
+			m_miners.back()->setWork(m_work);
+		}
+		m_isMining = true;
+		m_lastSealer = _sealer;
+		resetTimer();
+		return true;
+	}
 };
 
 }
