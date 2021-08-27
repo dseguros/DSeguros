b6d0ba35699499fbc0c768e62b513f5065316ddc
samanthacarl999 <samanthacarl999@hotmail.com>
2021-08-27 11:40:01 +0800

    dispatch

--- libevm/VMConfig.h
+++ libevm/VMConfig.h
@@ -117,4 +117,290 @@ namespace eth
 	#define DEFAULT default:
 	#define WHILE_CASES } }
 
+///////////////////////////////////////////////////////////////////////////////
+//
+// build an indirect-threaded interpreter using a jump table of
+// label addresses (a gcc extension)
+//
+#elif defined(EVM_JUMP_DISPATCH)
+
+	#define INIT_CASES  \
+	\
+		static const void * const jumpTable[256] =  \
+		{  \
+			&&STOP,          /* 00 */  \
+			&&ADD,  \
+			&&MUL,  \
+			&&SUB,  \
+			&&DIV,  \
+			&&SDIV,  \
+			&&MOD,  \
+			&&SMOD,  \
+			&&ADDMOD,  \
+			&&MULMOD,  \
+			&&EXP,  \
+			&&SIGNEXTEND,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&LT,            /* 10, */  \
+			&&GT,  \
+			&&SLT,  \
+			&&SGT,  \
+			&&EQ,  \
+			&&ISZERO,  \
+			&&AND,  \
+			&&OR,  \
+			&&XOR,  \
+			&&NOT,  \
+			&&BYTE,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&SHA3,          /* 20, */  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&ADDRESS,       /* 30, */  \
+			&&BALANCE,  \
+			&&ORIGIN,  \
+			&&CALLER,  \
+			&&CALLVALUE,  \
+			&&CALLDATALOAD,  \
+			&&CALLDATASIZE,  \
+			&&CALLDATACOPY,  \
+			&&CODESIZE,  \
+			&&CODECOPY,  \
+			&&GASPRICE,  \
+			&&EXTCODESIZE,  \
+			&&EXTCODECOPY,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&BLOCKHASH,     /* 40, */  \
+			&&COINBASE,  \
+			&&TIMESTAMP,  \
+			&&NUMBER,  \
+			&&DIFFICULTY,  \
+			&&GASLIMIT,  \
+			&&JUMPTO,  \
+			&&JUMPIF,  \
+			&&JUMPV,  \
+			&&JUMPSUB,  \
+			&&JUMPSUBV,  \
+			&&RETURNSUB,  \
+			&&BEGINSUB,  \
+			&&BEGINDATA,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&POP,           /* 50, */  \
+			&&MLOAD,  \
+			&&MSTORE,  \
+			&&MSTORE8,  \
+			&&SLOAD,  \
+			&&SSTORE,  \
+			&&JUMP,  \
+			&&JUMPI,  \
+			&&PC,  \
+			&&MSIZE,  \
+			&&GAS,  \
+			&&JUMPDEST,  \
+			&&BEGINDATA,  \
+			&&BEGINSUB,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&PUSH1,         /* 60, */  \
+			&&PUSH2,  \
+			&&PUSH3,  \
+			&&PUSH4,  \
+			&&PUSH5,  \
+			&&PUSH6,  \
+			&&PUSH7,  \
+			&&PUSH8,  \
+			&&PUSH9,  \
+			&&PUSH10,  \
+			&&PUSH11,  \
+			&&PUSH12,  \
+			&&PUSH13,  \
+			&&PUSH14,  \
+			&&PUSH15,  \
+			&&PUSH16,  \
+			&&PUSH17,         /* 70, */  \
+			&&PUSH18,  \
+			&&PUSH19,  \
+			&&PUSH20,  \
+			&&PUSH21,  \
+			&&PUSH22,  \
+			&&PUSH23,  \
+			&&PUSH24,  \
+			&&PUSH25,  \
+			&&PUSH26,  \
+			&&PUSH27,  \
+			&&PUSH28,  \
+			&&PUSH29,  \
+			&&PUSH30,  \
+			&&PUSH31,  \
+			&&PUSH32,  \
+			&&DUP1,          /* 80, */  \
+			&&DUP2,  \
+			&&DUP3,  \
+			&&DUP4,  \
+			&&DUP5,  \
+			&&DUP6,  \
+			&&DUP7,  \
+			&&DUP8,  \
+			&&DUP9,  \
+			&&DUP10,  \
+			&&DUP11,  \
+			&&DUP12,  \
+			&&DUP13,  \
+			&&DUP14,  \
+			&&DUP15,  \
+			&&DUP16,  \
+			&&SWAP1,         /* 90, */  \
+			&&SWAP2,  \
+			&&SWAP3,  \
+			&&SWAP4,  \
+			&&SWAP5,  \
+			&&SWAP6,  \
+			&&SWAP7,  \
+			&&SWAP8,  \
+			&&SWAP9,  \
+			&&SWAP10,  \
+			&&SWAP11,  \
+			&&SWAP12,  \
+			&&SWAP13,  \
+			&&SWAP14,  \
+			&&SWAP15,  \
+			&&SWAP16,  \
+			&&LOG0,          /* A0, */  \
+			&&LOG1,  \
+			&&LOG2,  \
+			&&LOG3,  \
+			&&LOG4,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&PUSHC,  \
+			&&JUMPC,  \
+			&&JUMPCI,  \
+			&&BAD,  \
+			&&INVALID,       /* B0, */  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,       /* C0, */  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,       /* D0, */  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,       /* E0, */  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&CREATE,        /* F0, */  \
+			&&CALL,  \
+			&&CALLCODE,  \
+			&&RETURN,  \
+			&&DELEGATECALL,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&INVALID,  \
+			&&SUICIDE,  \
+		};  \
+		if (!m_caseInit) {            \
+			c_jumpTable = jumpTable;  \
+			m_PC = 0;                 \
+			m_caseInit = true;        \
+			return;                   \
+		}
+
+	#define DO_CASES fetchInstruction(); goto *jumpTable[(int)m_OP];
+	#define CASE(name) name:
+	#define NEXT m_PC = 0; fetchInstruction(); goto *jumpTable[m_code[m_PC]];
+	#define CONTINUE fetchInstruction(); goto *jumpTable[m_code[m_PC]];
+	#define BREAK return;
+	#define DEFAULT INVALID:
+	#define WHILE_CASES
+
+#else
+	#error No opcode dispatch configured
+#endif
 }}
