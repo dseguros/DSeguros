73fc9b6c062e3de698cc86ca30062f2bb37b4d63
christopherdog <christopherdog@hotmail.com>
2021-08-30 07:20:01 +0800

    GenericMiner.h

---
+++ libethereum/GenericMiner.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <libdevcore/Common.h>
+#include <libdevcore/Log.h>
+#include <libdevcore/Worker.h>
+#include <libethcore/Common.h>
+
+namespace dev
+{
+
+namespace eth
+{
+
+}
+}
