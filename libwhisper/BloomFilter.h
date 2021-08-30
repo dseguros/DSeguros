85d9403ebdab2e9efd9303d80bc6ffa5603763b8
samanthacarl999 <samanthacarl999@hotmail.com>
2021-08-31 06:30:01 +0800

    TopicBloomFilterBase

--- libwhisper/BloomFilter.h
+++ libwhisper/BloomFilter.h
@@ -7,6 +7,18 @@ namespace dev
 namespace shh
 {
 
+template <unsigned N>
+class TopicBloomFilterBase: public FixedHash<N>
+{
+public:
+	TopicBloomFilterBase() { init(); }
+	TopicBloomFilterBase(FixedHash<N> const& _h): FixedHash<N>(_h) { init(); }
+
+	void addBloom(AbridgedTopic const& _h) { addRaw(bloom(_h)); }
+	void removeBloom(AbridgedTopic const& _h) { removeRaw(bloom(_h)); }
+	bool containsBloom(AbridgedTopic const& _h) const { return this->contains(bloom(_h)); }
+
+};
 
 }
 }
