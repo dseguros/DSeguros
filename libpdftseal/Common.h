4375c5a40c32e0174343ede6cb004c9c0b7e1876
samanthacarl999 <samanthacarl999@hotmail.com>
2021-09-02 07:20:01 +0800

    Add common.h

---
+++ libpdftseal/Common.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <libdevcore/concurrent_queue.h>
+//#include <libdevcore/easylog.h>
+#include <libdevcore/RLP.h>
+#include <libdevcore/SHA3.h>
+#include <libdevcrypto/Common.h>
+#include <libethcore/Exceptions.h>
+
+namespace dev
+{
+
+namespace eth
+{
+
+}
+}
