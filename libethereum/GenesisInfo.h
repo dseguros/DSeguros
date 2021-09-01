88f32b75c5a62c3bd271afc634f06707d6654abc
christopherdog <christopherdog@hotmail.com>
2021-09-01 09:00:01 +0800

    GenesisInfo.h

---
+++ libethereum/GenesisInfo.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+#include <libdevcrypto/Common.h>
+
+namespace dev
+{
+namespace eth
+{
+
+extern std::string const c_genesisInfoTestBasicAuthority;
+extern dev::Addresses childDaos();
+
+}
+}
