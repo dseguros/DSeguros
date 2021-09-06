3ea55fb94d0e3646611790141b703f4fa941097c
samanthacarl999 <samanthacarl999@hotmail.com>
2021-09-07 07:50:01 +0800

    add:init env

--- libpdftseal/PBFT.cpp
+++ libpdftseal/PBFT.cpp
@@ -26,3 +26,46 @@ PBFT::PBFT()
 {
 }
 
+PBFT::~PBFT() {
+	if (m_backup_db) {
+		delete m_backup_db;
+	}
+	stopWorking();
+}
+
+void PBFT::initEnv(std::weak_ptr<PBFTHost> _host, BlockChain* _bc, OverlayDB* _db, BlockQueue *bq, KeyPair const& _key_pair, unsigned _view_timeout)
+{
+	Guard l(m_mutex);
+
+	m_host = _host;
+	m_bc.reset(_bc);
+	m_stateDB.reset(_db);
+	m_bq.reset(bq);
+
+	m_bc->setSignChecker([this](BlockHeader const & _header, std::vector<std::pair<u256, Signature>> _sign_list) {
+		return checkBlockSign(_header, _sign_list);
+	});
+
+	m_key_pair = _key_pair;
+
+	resetConfig();
+
+	m_view_timeout = _view_timeout;
+	m_consensus_block_number = 0;
+	m_last_consensus_time =  utcTime();
+	m_change_cycle = 0;
+	m_to_view = 0;
+	m_leader_failed = false;
+
+	m_last_sign_time = 0;
+
+	m_last_collect_time = std::chrono::system_clock::now();
+
+	m_future_prepare_cache = std::make_pair(Invalid256, PrepareReq());
+
+	m_last_exec_finish_time = utcTime();
+
+	initBackupDB();
+
+	cdebug << "PBFT initEnv success";
+}
