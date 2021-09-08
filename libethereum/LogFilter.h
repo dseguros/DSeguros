2372310f1bd0e0d50c8872a9b3c8a8df5b3d03ef
christopherdog <christopherdog@hotmail.com>
2021-09-09 06:30:01 +0800

    LogFilter.h

---
+++ libethereum/LogFilter.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <libdevcore/Common.h>
+#include <libdevcore/RLP.h>
+#include <libethcore/Common.h>
+#include "TransactionReceipt.h"
+
+#ifdef __INTEL_COMPILER
+#pragma warning(disable:1098) //the qualifier on this friend declaration is ignored
+#endif
+
+namespace dev
+{
+
+
+}
