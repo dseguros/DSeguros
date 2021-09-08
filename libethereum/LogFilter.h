737fc3e4cab04077cf9a0ffb3934c221db0ec426
christopherdog <christopherdog@hotmail.com>
2021-09-09 07:50:01 +0800

    streamRLP

--- libethereum/LogFilter.h
+++ libethereum/LogFilter.h
@@ -17,4 +17,26 @@ namespace eth
 class LogFilter;
 }
 
+
+namespace eth
+{
+
+/// Simple stream output for the StateDiff.
+std::ostream& operator<<(std::ostream& _out, dev::eth::LogFilter const& _s);
+
+class State;
+class Block;
+
+class LogFilter
+{
+public:
+	LogFilter(h256 _earliest = EarliestBlockHash, h256 _latest = PendingBlockHash): m_earliest(_earliest), m_latest(_latest) {}
+
+	void streamRLP(RLPStream& _s) const;
+	h256 sha3() const;
+
+};
+
+}
+
 }
