a8cb535d751d35290b871553f1163f220c4d9813
samanthacarl999 <samanthacarl999@hotmail.com>
2021-09-14 05:00:01 +0800

    PBFTClient.cpp

---
+++ libpdftseal/PBFTClient.cpp
@@ -0,0 +1,13 @@
+//#include <libdevcore/easylog.h>
+#include <libethereum/EthereumHost.h>
+//#include <libethereum/NodeConnParamsManagerApi.h>
+#include <libp2p/Host.h>
+#include "PBFTClient.h"
+#include "PBFT.h"
+#include "PBFTHost.h"
+//#include <libdevcore/easylog.h>
+//#include <libethereum/StatLog.h>
+using namespace std;
+using namespace dev;
+using namespace dev::eth;
+using namespace p2p;
