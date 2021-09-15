991ed600155eb635ca53978c41918d0d1b7025de
christopherdog <christopherdog@hotmail.com>
2021-09-15 09:00:01 +0800

    VerifiedBlockRef

--- libethereum/VerifiedBlock.h
+++ libethereum/VerifiedBlock.h
@@ -8,6 +8,15 @@ namespace dev
 namespace eth
 {
 
+class Transaction;
+
+/// @brief Verified block info, does not hold block data, but a reference instead
+struct VerifiedBlockRef
+{
+	bytesConstRef block; 					///<  Block data reference
+	BlockHeader info;							///< Prepopulated block info
+	std::vector<Transaction> transactions;	///< Verified list of block transactions
+};
 
 }
 }
