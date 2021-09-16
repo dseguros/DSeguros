90a41e353115cbe48681f29098666eb1900b6d8d
samanthacarl999 <samanthacarl999@hotmail.com>
2021-09-17 07:50:01 +0800

    HostCapability

--- libpdftseal/PBFTHost.h
+++ libpdftseal/PBFTHost.h
@@ -11,6 +11,15 @@ namespace dev
 namespace eth
 {
 
+class PBFTHost: public p2p::HostCapability<PBFTPeer>
+{
+public:
+	typedef std::function<void(unsigned, std::shared_ptr<p2p::Capability>, RLP const&)> MsgHandler;
+
+	PBFTHost(MsgHandler h): m_msg_handler(h) {}
+	virtual ~PBFTHost() {}
+
+};
 
 }
 }
