feb61afddfb8b296330b4e99eb6a93d2b3d44f38
christopherdog <christopherdog@hotmail.com>
2021-09-18 07:50:01 +0800

    ethash_io_create_filename

--- libethash/io.c
+++ libethash/io.c
@@ -16,5 +16,18 @@ enum ethash_io_rc ethash_io_prepare(
 	// reset errno before io calls
 	errno = 0;
 
+ 	// assert directory exists
+	if (!ethash_mkdir(dirname)) {
+		ETHASH_CRITICAL("Could not create the ethash directory");
+		goto end;
+	}
+
+	ethash_io_mutable_name(ETHASH_REVISION, &seedhash, mutable_name);
+	char* tmpfile = ethash_io_create_filename(dirname, mutable_name, strlen(mutable_name));
+	if (!tmpfile) {
+		ETHASH_CRITICAL("Could not create the full DAG pathname");
+		goto end;
+	}
+
 }
 
