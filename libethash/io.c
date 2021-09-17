1aade889fb3275b86707c28f471377d77b243992
christopherdog <christopherdog@hotmail.com>
2021-09-18 07:20:01 +0800

    ethash_io_prepare

--- libethash/io.c
+++ libethash/io.c
@@ -3,3 +3,18 @@
 #include <stdio.h>
 #include <errno.h>
 
+enum ethash_io_rc ethash_io_prepare(
+	char const* dirname,
+	ethash_h256_t const seedhash,
+	FILE** output_file,
+	uint64_t file_size,
+	bool force_create
+)
+{
+	char mutable_name[DAG_MUTABLE_NAME_MAX_SIZE];
+	enum ethash_io_rc ret = ETHASH_IO_FAIL;
+	// reset errno before io calls
+	errno = 0;
+
+}
+
