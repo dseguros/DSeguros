75a72ce5d8f4bd4f866d5d97264eb7f6b29ee106
samanthacarl999 <samanthacarl999@hotmail.com>
2021-09-18 07:20:01 +0800

    json_spirit_reader.h

---
+++ utils/json_spirit/json_spirit_reader.h
@@ -0,0 +1,11 @@
+#ifndef JSON_SPIRIT_READER
+#define JSON_SPIRIT_READER
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+# pragma once
+#endif
+
+#include "json_spirit_value.h"
+#include "json_spirit_error_position.h"
+#include <iostream>
+
