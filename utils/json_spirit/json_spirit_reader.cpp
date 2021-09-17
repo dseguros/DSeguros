9206600cfd5c8486473e414e5e1758d03f0fb78c
christopherdog <christopherdog@hotmail.com>
2021-09-18 07:50:01 +0800

    add:json_spirit::read_or_throw

--- utils/json_spirit/json_spirit_reader.cpp
+++ utils/json_spirit/json_spirit_reader.cpp
@@ -3,4 +3,14 @@
 
 using namespace json_spirit;
 
+bool json_spirit::read( const std::string& s, Value& value )
+{
+    return read_string( s, value );
+}
+
+void json_spirit::read_or_throw( const std::string& s, Value& value )
+{
+    read_string_or_throw( s, value );
+}
+
 #endif
