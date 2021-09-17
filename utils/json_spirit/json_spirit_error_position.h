fb3d8a08269c362c61dfb5fd9e37cd8efe286d27
samanthacarl999 <samanthacarl999@hotmail.com>
2021-09-18 07:20:01 +0800

    json_spirit_error_position.h

---
+++ utils/json_spirit/json_spirit_error_position.h
@@ -0,0 +1,11 @@
+#ifndef JSON_SPIRIT_ERROR_POSITION
+#define JSON_SPIRIT_ERROR_POSITION
+
+//          Copyright John W. Wilkinson 2007 - 2009.
+// Distributed under the MIT License, see accompanying file LICENSE.txt
+
+// json spirit version 4.03
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+# pragma once
+#endif
