6d0f0e4cf05c5f0186f7b1d8ca23632a8141fa6f
christopherdog <christopherdog@hotmail.com>
2021-09-18 05:50:01 +0800

    BlockQueue.h

---
+++ libethereum/BlockQueue.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <condition_variable>
+#include <thread>
+#include <deque>
+#include <boost/thread.hpp>
+#include <libdevcore/Common.h>
+#include <libdevcore/Log.h>
+#include <libethcore/Common.h>
+#include <libdevcore/Guards.h>
+#include <libethcore/Common.h>
+#include <libethcore/BlockHeader.h>
+#include "VerifiedBlock.h"
+
+namespace dev
+{
+
+namespace eth
+{
+
+
+}
+}
