13abd079d2459f78831b2634325ac3362e59fcae
samanthacarl999 <samanthacarl999@hotmail.com>
2021-09-17 10:10:01 +0800

    implements

---
+++ libpdftseal/PBFTHost.cpp
@@ -0,0 +1,12 @@
+#include "PBFTHost.h"
+using namespace std;
+using namespace dev;
+using namespace dev::eth;
+using namespace p2p;
+
+void PBFTHost::foreachPeer(std::function<bool(std::shared_ptr<PBFTPeer>)> const& _f) const
+{
+	
+}
+
+
