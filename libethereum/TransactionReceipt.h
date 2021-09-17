0b4ed7051efe4104b836620e6ff65687f6e6ef1e
christopherdog <christopherdog@hotmail.com>
2021-09-18 06:30:01 +0800

    add:stateRoot

--- libethereum/TransactionReceipt.h
+++ libethereum/TransactionReceipt.h
@@ -11,6 +11,18 @@ namespace dev
 namespace eth
 {
 
+class TransactionReceipt
+{
+public:
+	TransactionReceipt(bytesConstRef _rlp);
+	TransactionReceipt(h256 _root, u256 _gasUsed, LogEntries const& _log);
+
+	h256 const& stateRoot() const { return m_stateRoot; }
+	u256 const& gasUsed() const { return m_gasUsed; }
+	LogBloom const& bloom() const { return m_bloom; }
+	LogEntries const& log() const { return m_log; }
+
+};
 
 }
 }
