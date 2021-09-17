9dc8226264f6e14952136f5531b76c58d6a2fc39
samanthacarl999 <samanthacarl999@hotmail.com>
2021-09-18 05:50:01 +0800

    Interface.h

---
+++ libwhisper/Interface.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <mutex>
+#include <array>
+#include <set>
+#include <memory>
+#include <utility>
+
+#include <libdevcore/RLP.h>
+#include <libdevcore/Guards.h>
+#include <libdevcore/SHA3.h>
+#include "Common.h"
+#include "Message.h"
+
+namespace dev
+{
+namespace shh
+{
+
+
+}
+
+}
+
