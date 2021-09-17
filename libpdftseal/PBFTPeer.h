dd355aeb7b56bdfdda99911a2e96d280bdad3ac9
samanthacarl999 <samanthacarl999@hotmail.com>
2021-09-18 07:50:01 +0800

    p2p::NodeID

--- libpdftseal/PBFTPeer.h
+++ libpdftseal/PBFTPeer.h
@@ -19,6 +19,18 @@ class PBFTPeer : public p2p::Capability
 public:
 	PBFTPeer(std::shared_ptr<p2p::SessionFace> _s, p2p::HostCapabilityFace* _h, unsigned _i, p2p::CapDesc const& _cap, uint16_t _capID);
 
+/// Basic destructor.
+	virtual ~PBFTPeer();
+
+	/// What is our name?
+	static std::string name() { return "pbft"; }
+	/// What is our version?
+	static u256 version() { return c_protocolVersion; }
+	/// How many message types do we have?
+	static unsigned messageCount() { return PBFTPacketCount; }
+
+	p2p::NodeID id() const { return session()->id(); }
+
 };
 
 }
