a62d3ed26acca6aa74f469f53cf1b1f067a85463
samanthacarl999 <samanthacarl999@hotmail.com>
2021-09-18 06:30:01 +0800

    Transaction.h

---
+++ libethereum/Transaction.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <libdevcore/RLP.h>
+#include <libdevcore/SHA3.h>
+#include <libethcore/Common.h>
+#include <libethcore/Transaction.h>
+#include <libethcore/ChainOperationParams.h>
+
+namespace dev
+{
+namespace eth
+{
+
+
+}
+}
