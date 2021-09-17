29658c0caa03bf7659f46974256d90ac96ec14f9
samanthacarl999 <samanthacarl999@hotmail.com>
2021-09-18 07:20:01 +0800

    define:

--- libethereum/Transaction.h
+++ libethereum/Transaction.h
@@ -11,6 +11,26 @@ namespace dev
 namespace eth
 {
 
+enum class TransactionException
+{
+	None = 0,
+	Unknown,
+	BadRLP,
+	InvalidFormat,
+	OutOfGasIntrinsic,		///< Too little gas to pay for the base transaction cost.
+	InvalidSignature,
+	InvalidNonce,
+	NotEnoughCash,
+	OutOfGasBase,			///< Too little gas to pay for the base transaction cost.
+	BlockGasLimitReached,
+	BadInstruction,
+	BadJumpDestination,
+	OutOfGas,				///< Ran out of gas executing code of the transaction.
+	OutOfStack,				///< Ran out of stack executing code of the transaction.
+	StackUnderflow,
+	RevertInstruction,
+	InvalidZeroSignatureFormat
+};
 
 }
 }
