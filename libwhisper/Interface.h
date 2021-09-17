f4b9797e2d28264f8facc5e52bfee75a1857ff6e
samanthacarl999 <samanthacarl999@hotmail.com>
2021-09-18 07:50:01 +0800

    fix:inject

--- libwhisper/Interface.h
+++ libwhisper/Interface.h
@@ -37,6 +37,21 @@ struct ClientWatch
 	h256s changes;
 };
 
+class Interface
+{
+public:
+	virtual ~Interface();
+
+	virtual void inject(Envelope const& _m, WhisperPeer* _from = nullptr) = 0;
+
+	virtual Topics const& fullTopics(unsigned _id) const = 0;
+	virtual unsigned installWatch(Topics const& _filter) = 0;
+	virtual void uninstallWatch(unsigned _watchId) = 0;
+	virtual h256s peekWatch(unsigned _watchId) const = 0;
+	virtual h256s checkWatch(unsigned _watchId) = 0;
+	virtual h256s watchMessages(unsigned _watchId) = 0;
+
+};
 }
 
 }
