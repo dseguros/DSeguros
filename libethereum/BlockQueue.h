f25533fbcdd4c36eb494674ff1105efd74525502
christopherdog <christopherdog@hotmail.com>
2021-09-18 06:30:01 +0800

    add:BlockQueueStatus

--- libethereum/BlockQueue.h
+++ libethereum/BlockQueue.h
@@ -18,6 +18,22 @@ namespace dev
 namespace eth
 {
 
+class BlockChain;
+
+struct BlockQueueChannel: public LogChannel { static const char* name(); static const int verbosity = 4; };
+struct BlockQueueTraceChannel: public LogChannel { static const char* name(); static const int verbosity = 7; };
+#define cblockq dev::LogOutputStream<dev::eth::BlockQueueChannel, true>()
+
+struct BlockQueueStatus
+{
+	size_t importing;
+	size_t verified;
+	size_t verifying;
+	size_t unverified;
+	size_t future;
+	size_t unknown;
+	size_t bad;
+};
 
 }
 }
