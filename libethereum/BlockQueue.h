2c59f77d807bf125668701662c2da808e372c052
christopherdog <christopherdog@hotmail.com>
2021-09-18 11:40:01 +0800

    add:new class

--- libethereum/BlockQueue.h
+++ libethereum/BlockQueue.h
@@ -195,6 +195,110 @@ private:
 	BlockMultimap m_map;
 	std::atomic<size_t> m_size = {0};	///< Tracks total size in bytes
 };
+class BlockQueue: HasInvariants
+{
+public:
+	BlockQueue();
+	~BlockQueue();
 
+	void setChain(BlockChain const& _bc) { m_bc = &_bc; }
+
+	/// Import a block into the queue.
+	ImportResult import(bytesConstRef _block, bool _isOurs = false);
+
+	/// Notes that time has moved on and some blocks that used to be "in the future" may no be valid.
+	void tick();
+
+	/// Grabs at most @a _max of the blocks that are ready, giving them in the correct order for insertion into the chain.
+	/// Don't forget to call doneDrain() once you're done importing.
+	void drain(std::vector<VerifiedBlock>& o_out, unsigned _max);
+
+	/// Must be called after a drain() call. Notes that the drained blocks have been imported into the blockchain, so we can forget about them.
+	/// @returns true iff there are additional blocks ready to be processed.
+	bool doneDrain(h256s const& _knownBad = h256s());
+
+	/// Notify the queue that the chain has changed and a new block has attained 'ready' status (i.e. is in the chain).
+	void noteReady(h256 const& _b) { WriteGuard l(m_lock); noteReady_WITH_LOCK(_b); }
+
+	/// Force a retry of all the blocks with unknown parents.
+	void retryAllUnknown();
+
+	/// Get information on the items queued.
+	std::pair<unsigned, unsigned> items() const { ReadGuard l(m_lock); return std::make_pair(m_readySet.size(), m_unknownSet.size()); }
+
+	/// Clear everything.
+	void clear();
+
+	/// Stop all activity, leaves the class in limbo, waiting for destruction
+	void stop();
+
+	/// Return first block with an unknown parent.
+	h256 firstUnknown() const { ReadGuard l(m_lock); return m_unknownSet.size() ? *m_unknownSet.begin() : h256(); }
+
+	/// Get some infomration on the current status.
+	BlockQueueStatus status() const;
+
+	/// Get some infomration on the given block's status regarding us.
+	QueueStatus blockStatus(h256 const& _h) const;
+
+	template <class T> Handler<> onReady(T const& _t) { return m_onReady.add(_t); }
+	template <class T> Handler<> onRoomAvailable(T const& _t) { return m_onRoomAvailable.add(_t); }
+
+	template <class T> void setOnBad(T const& _t) { m_onBad = _t; }
+
+	bool knownFull() const;
+	bool unknownFull() const;
+	u256 difficulty() const;	// Total difficulty of queueud blocks
+	bool isActive() const;
+
+private:
+	struct UnverifiedBlock
+	{
+		h256 hash;
+		h256 parentHash;
+		bytes blockData;
+	};
+
+	void noteReady_WITH_LOCK(h256 const& _b);
+
+	bool invariants() const override;
+
+	void verifierBody();
+	void collectUnknownBad_WITH_BOTH_LOCKS(h256 const& _bad);
+	void updateBad_WITH_LOCK(h256 const& _bad);
+	void drainVerified_WITH_BOTH_LOCKS();
+
+	std::size_t knownSize() const;
+	std::size_t knownCount() const;
+	std::size_t unknownSize() const;
+	std::size_t unknownCount() const;
+
+	BlockChain const* m_bc;												///< The blockchain into which our imports go.
+
+	mutable boost::shared_mutex m_lock;									///< General lock for the sets, m_future and m_unknown.
+	h256Hash m_drainingSet;												///< All blocks being imported.
+	h256Hash m_readySet;												///< All blocks ready for chain import.
+	h256Hash m_unknownSet;												///< Set of all blocks whose parents are not ready/in-chain.
+	SizedBlockMap<h256> m_unknown;										///< For blocks that have an unknown parent; we map their parent hash to the block stuff, and insert once the block appears.
+	h256Hash m_knownBad;												///< Set of blocks that we know will never be valid.
+	SizedBlockMap<time_t> m_future;										///< Set of blocks that are not yet valid. Ordered by timestamp
+	Signal<> m_onReady;													///< Called when a subsequent call to import blocks will return a non-empty container. Be nice and exit fast.
+	Signal<> m_onRoomAvailable;											///< Called when space for new blocks becomes availabe after a drain. Be nice and exit fast.
+
+	mutable Mutex m_verification;										///< Mutex that allows writing to m_verified, m_verifying and m_unverified.
+	std::condition_variable m_moreToVerify;								///< Signaled when m_unverified has a new entry.
+	SizedBlockQueue<VerifiedBlock> m_verified;								///< List of blocks, in correct order, verified and ready for chain-import.
+	SizedBlockQueue<VerifiedBlock> m_verifying;								///< List of blocks being verified; as long as the block component (bytes) is empty, it's not finished.
+	SizedBlockQueue<UnverifiedBlock> m_unverified;							///< List of <block hash, parent hash, block data> in correct order, ready for verification.
+
+	std::vector<std::thread> m_verifiers;								///< Threads who only verify.
+	std::atomic<bool> m_deleting = {false};								///< Exit condition for verifiers.
+
+	std::function<void(Exception&)> m_onBad;							///< Called if we have a block that doesn't verify.
+	u256 m_difficulty;													///< Total difficulty of blocks in the queue
+	u256 m_drainingDifficulty;											///< Total difficulty of blocks in draining
+};
+
+std::ostream& operator<<(std::ostream& _out, BlockQueueStatus const& _s);
 }
 }
