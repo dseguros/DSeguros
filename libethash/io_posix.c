07bf124426f16896ca8ee5b856be7d9ee3ad0899
samanthacarl999 <samanthacarl999@hotmail.com>
2021-10-19 09:20:01 +0800

    ethash_io_create_filename

--- libethash/io_posix.c
+++ libethash/io_posix.c
@@ -33,3 +33,29 @@ int ethash_fileno(FILE *f)
 {
 	return fileno(f);
 }
+
+char* ethash_io_create_filename(
+	char const* dirname,
+	char const* filename,
+	size_t filename_length
+)
+{
+	size_t dirlen = strlen(dirname);
+	size_t dest_size = dirlen + filename_length + 1;
+	if (dirname[dirlen] != '/') {
+		dest_size += 1;
+	}
+	char* name = malloc(dest_size);
+	if (!name) {
+		return NULL;
+	}
+
+	name[0] = '\0';
+	ethash_strncat(name, dest_size, dirname, dirlen);
+	if (dirname[dirlen] != '/') {
+		ethash_strncat(name, dest_size, "/", 1);
+	}
+	ethash_strncat(name, dest_size, filename, filename_length);
+	return name;
+}
+
