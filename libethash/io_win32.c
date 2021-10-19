546e7ea8b47d9c413eaee90b5ce99c70ac5159b2
christopherdog <christopherdog@hotmail.com>
2021-10-20 06:30:01 +0800

    ethash_fopen

--- libethash/io_win32.c
+++ libethash/io_win32.c
@@ -6,3 +6,10 @@
 #include <sys/types.h>
 #include <shlobj.h>
 #include <io.h>
+
+FILE* ethash_fopen(char const* file_name, char const* mode)
+{
+	FILE* f;
+	return fopen_s(&f, file_name, mode) == 0 ? f : NULL;
+}
+
