25653d6f739b99fcb6e652cb58620ad65af08e37
christopherdog <christopherdog@hotmail.com>
2021-10-22 07:20:01 +0800

    sha3.c

---
+++ sha3.c
@@ -0,0 +1,15 @@
+/** libkeccak-tiny
+*
+* A single-file implementation of SHA-3 and SHAKE.
+*
+* Implementor: David Leon Gil
+* License: CC0, attribution kindly requested. Blame taken too,
+* but not liability.
+*/
+#include "sha3.h"
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
