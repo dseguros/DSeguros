e445da31c43af64e228550de2050a90e5ca3383a
christopherdog <christopherdog@hotmail.com>
2021-10-29 07:20:01 +0800

    EthashAux.h

---
+++ libethashseal/EthashAux.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <condition_variable>
+#include <libethash/ethash.h>
+#include <libdevcore/Log.h>
+#include <libdevcore/Worker.h>
+#include "EthashProofOfWork.h"
+#include "Ethash.h"
+
+namespace dev
+{
+namespace eth
+{
+
+
+
+}
+}
