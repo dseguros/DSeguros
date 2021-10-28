69558fac56d6b4d5f8026c6683c4f9e2cf34ed82
samanthacarl999 <samanthacarl999@hotmail.com>
2021-10-28 11:40:01 +0800

    methods:v

--- libethash/util_win32.c
+++ libethash/util_win32.c
@@ -1,6 +1,16 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include "util.h"
+spec(dllimport) void __stdcall OutputDebugStringA(char const* lpOutputString);
 
+void debugf(char const* str, ...)
+{
+	va_list args;
+	va_start(args, str);
 
-
+	char buf[1<<16];
+	_vsnprintf_s(buf, sizeof(buf), sizeof(buf), str, args);
+	buf[sizeof(buf)-1] = '\0';
+	OutputDebugStringA(buf);
+}
+i
