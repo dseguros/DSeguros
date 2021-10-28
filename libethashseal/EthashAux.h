7dfdd41906737426b76347577a411e2a7e48a2b2
christopherdog <christopherdog@hotmail.com>
2021-10-29 07:50:01 +0800

    add:EthashAux

--- libethashseal/EthashAux.h
+++ libethashseal/EthashAux.h
@@ -12,6 +12,27 @@ namespace dev
 namespace eth
 {
 
+struct DAGChannel: public LogChannel { static const char* name(); static const int verbosity = 1; };
+
+class BlockHeader;
+
+class EthashAux
+{
+public:
+	~EthashAux();
+
+	static EthashAux* get();
+
+	struct LightAllocation
+	{
+		LightAllocation(h256 const& _seedHash);
+		~LightAllocation();
+		bytesConstRef data() const;
+		EthashProofOfWork::Result compute(h256 const& _headerHash, Nonce const& _nonce) const;
+		ethash_light_t light;
+		uint64_t size;
+	};
+};
 
 
 }
