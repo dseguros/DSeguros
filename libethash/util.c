0bf4ddc4c3175fa25112cfa1908f1775a7cbd3d5
samanthacarl999 <samanthacarl999@hotmail.com>
2021-10-28 09:20:01 +0800

    add:debugf

--- libethash/util.c
+++ libethash/util.c
@@ -7,6 +7,15 @@
 // foward declare without all of Windows.h
 __declspec(dllimport) void __stdcall OutputDebugStringA(const char* lpOutputString);
 
+void debugf(const char *str, ...)
+{
+	va_list args;
+    va_start(args, str);
 
+	char buf[1<<16];
+	_vsnprintf_s(buf, sizeof(buf), sizeof(buf), str, args);
+	buf[sizeof(buf)-1] = '\0';
+	OutputDebugStringA(buf);
+}
 
 #endif
